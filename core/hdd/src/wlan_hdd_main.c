34b2145a3c1469077c129fa634d8b5c0cca1b38a
Rajeev Kumar <rajekuma@codeaurora.org>
2017-03-31 20:25:23 -0700

    qcacld-3.0: Handle module stop properly for FTM mode
    In FTM mode con mode change handler does module stop
    after holding SSR protect and module stop bails
    out assuming its idle mode module stop. Make sure to
    do module stop properly for FTM mode even if SSR
    protect lock is taken by con mode change handler.
    
    Change-Id: I5d2d7006bb9f4897a5dd22893cdf6a7a864de97b
    CRs-Fixed: 2027020

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -5213,7 +5213,7 @@ static void hdd_wlan_exit(hdd_context_t *hdd_ctx)
 		QDF_ASSERT(QDF_IS_STATUS_SUCCESS(qdf_status));
 	}
 
-	hdd_wlan_stop_modules(hdd_ctx);
+	hdd_wlan_stop_modules(hdd_ctx, false);
 
 	qdf_nbuf_deinit_replenish_timer();
 
@@ -8450,6 +8450,7 @@ static int hdd_deconfigure_cds(hdd_context_t *hdd_ctx)
 /**
  * hdd_wlan_stop_modules - Single driver state machine for stoping modules
  * @hdd_ctx: HDD context
+ * @ftm_mode: ftm mode
  *
  * This function maintains the driver state machine it will be invoked from
  * exit, shutdown and con_mode change handler. Depending on the driver state
@@ -8457,7 +8458,7 @@ static int hdd_deconfigure_cds(hdd_context_t *hdd_ctx)
  *
  * Return: 0 for success; non-zero for failure
  */
-int hdd_wlan_stop_modules(hdd_context_t *hdd_ctx)
+int hdd_wlan_stop_modules(hdd_context_t *hdd_ctx, bool ftm_mode)
 {
 	void *hif_ctx;
 	qdf_device_t qdf_ctx;
@@ -8487,10 +8488,11 @@ int hdd_wlan_stop_modules(hdd_context_t *hdd_ctx)
 
 		cds_print_external_threads();
 
-		if (is_idle_stop) {
+		if (is_idle_stop && !ftm_mode) {
 			mutex_unlock(&hdd_ctx->iface_change_lock);
 			qdf_mc_timer_start(&hdd_ctx->iface_change_timer,
-				       hdd_ctx->config->iface_change_wait_time);
+				hdd_ctx->config->iface_change_wait_time);
+			hdd_ctx->stop_modules_in_progress = false;
 			return 0;
 		}
 	}
@@ -8595,7 +8597,7 @@ static void hdd_iface_change_callback(void *priv)
 
 	ENTER();
 	hdd_info("Interface change timer expired close the modules!");
-	ret = hdd_wlan_stop_modules(hdd_ctx);
+	ret = hdd_wlan_stop_modules(hdd_ctx, false);
 	if (ret)
 		hdd_alert("Failed to stop modules");
 	EXIT();
@@ -8821,7 +8823,7 @@ err_wiphy_unregister:
 	wiphy_unregister(hdd_ctx->wiphy);
 
 err_stop_modules:
-	hdd_wlan_stop_modules(hdd_ctx);
+	hdd_wlan_stop_modules(hdd_ctx, false);
 
 err_exit_nl_srv:
 	if (DRIVER_MODULES_CLOSED == hdd_ctx->driver_status) {
@@ -10484,7 +10486,7 @@ static int __con_mode_handler(const char *kmessage, struct kernel_param *kp,
 	/* Cleanup present mode before switching to new mode */
 	hdd_cleanup_present_mode(hdd_ctx, curr_mode);
 
-	ret = hdd_wlan_stop_modules(hdd_ctx);
+	ret = hdd_wlan_stop_modules(hdd_ctx, true);
 	if (ret) {
 		hdd_err("Stop wlan modules failed");
 		goto reset_flags;
