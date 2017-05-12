3c653eb7f2df9d1919cb2f46cd279dbed7dfeb49
Paul Zhang <paulz@codeaurora.org>
2017-05-12 19:20:37 +0800

    qcacld-3.0: Associate in 1x1 when connected to a certain Vendor AP
    When connected to a certain Vendor AP that cannot handle SMPS frame, only
    connect in 1x1.
    
    Change-Id: Ieae25191df8340a582e81dc716b9a1aee6a71921
    CRs-Fixed: 2047347

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -3248,7 +3248,7 @@ static void wma_add_bss_ap_mode(tp_wma_handle wma, tpAddBssParams add_bss)
 	if (!QDF_IS_STATUS_SUCCESS(status))
 		WMA_LOGE("wma_get_current_hw_mode failed");
 
-	if ((add_bss->nss == 2) && !hw_mode.dbs_cap) {
+	if (add_bss->nss == 2) {
 		req.preferred_rx_streams = 2;
 		req.preferred_tx_streams = 2;
 	} else {
@@ -3393,7 +3393,7 @@ static void wma_add_bss_ibss_mode(tp_wma_handle wma, tpAddBssParams add_bss)
 	if (!QDF_IS_STATUS_SUCCESS(status))
 		WMA_LOGE("wma_get_current_hw_mode failed");
 
-	if ((add_bss->nss == 2) && !hw_mode.dbs_cap) {
+	if (add_bss->nss == 2) {
 		req.preferred_rx_streams = 2;
 		req.preferred_tx_streams = 2;
 	} else {
@@ -3574,7 +3574,7 @@ static void wma_add_bss_sta_mode(tp_wma_handle wma, tpAddBssParams add_bss)
 			if (!QDF_IS_STATUS_SUCCESS(status))
 				WMA_LOGE("wma_get_current_hw_mode failed");
 
-			if ((add_bss->nss == 2) && !hw_mode.dbs_cap) {
+			if (add_bss->nss == 2) {
 				req.preferred_rx_streams = 2;
 				req.preferred_tx_streams = 2;
 			} else {
