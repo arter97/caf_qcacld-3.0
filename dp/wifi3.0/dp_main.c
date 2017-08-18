f009865b8c3cd72644b8d9346a167225f7dd21cd
Venkateswara Swamy Bandaru <vbandaru@codeaurora.org>
2017-08-18 19:13:10 +0530

    qcacmn: Add support to pass napi budget as module parameter
    Add module parameter to pass napi budget for dp interrupts. Default value is
    set to 128.
    
    Change-Id: I6b3761c6908ed9cec4e40118a657b39da6ac2532
    CRs-Fixed: 2095242

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -58,6 +58,11 @@ cdp_dump_flow_pool_info(struct cdp_soc_t *soc)
 #define DP_CURR_FW_STATS_AVAIL 19
 #define DP_HTT_DBG_EXT_STATS_MAX 256
 
+unsigned int napi_budget = 128;
+module_param(napi_budget, uint, 0644);
+MODULE_PARM_DESC(napi_budget,
+                "tasklet mode: more than 0xffff , napi budget if <= 0xffff");
+
 /**
  * default_dscp_tid_map - Default DSCP-TID mapping
  *
@@ -635,7 +640,7 @@ static QDF_STATUS dp_soc_interrupt_attach(void *txrx_soc)
 		ret = hif_register_ext_group(soc->hif_handle,
 				num_irq, irq_id_map, dp_service_srngs,
 				&soc->intr_ctx[i], "dp_intr",
-				HIF_EXEC_NAPI_TYPE);
+				napi_budget);
 
 		if (ret) {
 			QDF_TRACE(QDF_MODULE_ID_DP, QDF_TRACE_LEVEL_ERROR,
