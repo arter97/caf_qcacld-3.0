ae7ea2a494295b71a9a999f40f29c982b5258d64
Jeff Johnson <jjohnson@codeaurora.org>
2017-09-18 08:01:50 -0700

    qcacld-3.0: mac: Replace instances of unadorned %p
    Replace instances of unadorned %p in core/mac
    
    Change-Id: Id9b15d369dda7660f292e6c961b25d2e8270b141
    CRs-Fixed: 2100997

--- core/mac/src/pe/lim/lim_utils.c
+++ core/mac/src/pe/lim/lim_utils.c
@@ -6170,7 +6170,7 @@ void lim_set_ht_caps(tpAniSirGlobal p_mac, tpPESession p_session_entry,
 	populate_dot11f_ht_caps(p_mac, p_session_entry, &dot11_ht_cap);
 	p_ie = lim_get_ie_ptr_new(p_mac, p_ie_start, num_bytes,
 			DOT11F_EID_HTCAPS, ONE_BYTE);
-	pe_debug("p_ie: %p dot11_ht_cap.supportedMCSSet[0]: 0x%x",
+	pe_debug("p_ie: %pK dot11_ht_cap.supportedMCSSet[0]: 0x%x",
 		p_ie, dot11_ht_cap.supportedMCSSet[0]);
 	if (p_ie) {
 		/* convert from unpacked to packed structure */
