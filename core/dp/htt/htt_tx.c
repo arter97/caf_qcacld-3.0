OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: dp: Replace instances of unadorned %p
Replace instances of unadorned %p in core/dp.

Change-Id: I7baa3ecc4f9964c47261dafa26116447fc7611b5
CRs-Fixed: 2100997

core/dp/htt/htt_tx.c
@@ -1021,7 +1021,7 @@ void htt_tx_desc_display(void *tx_desc)
 	htt_tx_desc = (struct htt_tx_msdu_desc_t *)tx_desc;
 
 	/* only works for little-endian */
-	qdf_debug("HTT tx desc (@ %p):", htt_tx_desc);
+	qdf_debug("HTT tx desc (@ %pK):", htt_tx_desc);
 	qdf_debug("  msg type = %d", htt_tx_desc->msg_type);
 	qdf_debug("  pkt subtype = %d", htt_tx_desc->pkt_subtype);
 	qdf_debug("  pkt type = %d", htt_tx_desc->pkt_type);

arter97/caf_qcacld-3.0

core/dp/htt/htt_tx.c
@@ -1021,7 +1021,7 @@ void htt_tx_desc_display(void *tx_desc)
 	htt_tx_desc = (struct htt_tx_msdu_desc_t *)tx_desc;
 
 	/* only works for little-endian */
-	qdf_print("HTT tx desc (@ %p):", htt_tx_desc);
+	qdf_print("HTT tx desc (@ %pK):", htt_tx_desc);
 	qdf_print("  msg type = %d", htt_tx_desc->msg_type);
 	qdf_print("  pkt subtype = %d", htt_tx_desc->pkt_subtype);
 	qdf_print("  pkt type = %d", htt_tx_desc->pkt_type);