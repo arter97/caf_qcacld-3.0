aefd65c746624921b1d23a17b6f73a6e56d34cde
Nandha Kishore Easwaran <nandhaki@codeaurora.org>
2018-03-12 18:27:10 +0530

    qcacmn: Avoid mcast check in qwrap isolation mode
    Donot perform mcast echo check in case of qwrap isolation mode.
    In qwrap isolation mode, packets from wired station reaches the wireless
    station via the rootap. Thus we need to allow loopback packets in qwrap
    isolation mode.
    Also add a configuration to set isolation mode.
    
    Change-Id: If4a63abd477596f6b48c6cf9b6e7e5bd84ec95c0
    CRs-fixed: 2204033

--- dp/inc/cdp_txrx_stats_struct.h
+++ dp/inc/cdp_txrx_stats_struct.h
@@ -1232,6 +1232,7 @@ enum _ol_ath_param_t {
 	OL_ATH_PARAM_TXCHAINSOFT = 361,
 	OL_ATH_PARAM_WIDE_BAND_SCAN = 362,
 	OL_ATH_PARAM_CCK_TX_ENABLE = 363,
+	OL_ATH_PARAM_ISOLATION = 365,
 };
 
 /* Enumeration of PDEV Configuration parameter */
