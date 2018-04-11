9ff8ac660cb181f5fac7077aa92bd4d36726ffaa
Dustin Brown <dustinb@codeaurora.org>
2018-04-11 16:09:48 -0700

    qcacmn: Fix NBUF_MEMORY_DEBUG PERF build error
    qdf_net_buf_debug_init/exit() are defined when NBUF_MEMORY_DEBUG is
    disabled, but they are not declared in the nbuf header file, leading
    to build errors for PERF builds. Make qdf_net_buf_debug_init/exit()
    static inline stubs when NBUF_MEMORY_DEBUG is disabled.
    
    Change-Id: I4180086f27873a48cb965abed21f4bca2a4ca3a1
    CRs-Fixed: 2222969

--- qdf/linux/src/qdf_nbuf.c
+++ qdf/linux/src/qdf_nbuf.c
@@ -2310,20 +2310,8 @@ free_buf:
 }
 qdf_export_symbol(qdf_nbuf_free_debug);
 
-#else
-void qdf_net_buf_debug_init(void)
-{
-}
+#endif /* NBUF_MEMORY_DEBUG */
 
-qdf_export_symbol(qdf_net_buf_debug_init);
-
-void qdf_net_buf_debug_exit(void)
-{
-}
-
-qdf_export_symbol(qdf_net_buf_debug_exit);
-
-#endif /*NBUF_MEMORY_DEBUG */
 #if defined(FEATURE_TSO)
 
 /**
