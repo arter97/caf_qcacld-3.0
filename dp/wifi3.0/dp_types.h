c319b424108eed6e9746e79fea9bf0b1905cadf7
Chaithanya Garrepalli <cgarre@codeaurora.org>
2018-04-11 17:49:28 +0530

    qcacmn: Initialize dp peer map memory after WMI service ready
    Initialize the peer map and peer hash memory based on the config
    received from target in service ready event
    
    Change-Id: I8e1b62cd9586f5b2be7acb863a106d2486be1b35
    CRs-Fixed: 2223428

--- dp/wifi3.0/dp_types.h
+++ dp/wifi3.0/dp_types.h
@@ -788,7 +788,7 @@ struct dp_soc {
 	DP_MUTEX_TYPE peer_ref_mutex;
 
 	/* maximum value for peer_id */
-	int max_peers;
+	uint32_t max_peers;
 
 	/* SoC level data path statistics */
 	struct dp_soc_stats stats;
