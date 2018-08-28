bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Stop Roaming before handing CSA for STA
Currently if the FW sends a Roam Start while the host is handling the
CSA Offload event and triggering a vdev_restart, the FW asserts
when the it receives the vdev_restart as it has already started the
Roam.

Disable roaming by sending RSO Stop when the CSA offload event is
received from the FW and restart RSO when channel switch is complete.

Change-ID: I33e9ce6307bc36994eb5f4a99434cb76ce5d3184
CRs-Fixed: 2289047

core/mac/inc/ani_global.h
@@ -759,6 +759,8 @@ typedef struct sAniSirLim {
 	uint8_t gLimDfsTargetChanNum;
 	QDF_STATUS(*sme_msg_callback)
 		(tpAniSirGlobal mac, struct scheduler_msg *msg);
+	QDF_STATUS(*stop_roaming_callback)
+		(tpAniSirGlobal mac, uint8_t session_id, uint8_t reason);
 	uint8_t retry_packet_cnt;
 	uint8_t beacon_probe_rsp_cnt_per_scan;
 	wlan_scan_requester req_id;

arter97/caf_qcacld-3.0

core/mac/inc/ani_global.h
@@ -834,6 +834,8 @@ typedef struct sAniSirLim {
 		uint32_t scan_id, uint32_t flags);
 	QDF_STATUS(*sme_msg_callback)
 		(tHalHandle hal, cds_msg_t *msg);
+	QDF_STATUS(*stop_roaming_callback)
+		(tHalHandle hal, uint8_t session_id, uint8_t reason);
 	uint8_t retry_packet_cnt;
 	uint8_t scan_disabled;
 	uint8_t beacon_probe_rsp_cnt_per_scan;