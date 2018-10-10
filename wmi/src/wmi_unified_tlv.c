825c1cf0a4209b0bb69236ac9178e1db705ba19d
Nandha Kishore Easwaran <nandhaki@codeaurora.org>
2018-10-10 11:04:09 +0530

    qcacmn: Change pdev_stats to pdev_stats_v2
    Change the wmi_pdev_stats structure to wmi_pdev_stats_v2 structure.
    This change is needed because of corresponding change made in FW
    for renaming the structure.
    
    Change-Id: I6dd3abd61730d8f17d74a11a42978a64853136e5

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -19301,7 +19301,7 @@ static QDF_STATUS extract_all_stats_counts_tlv(wmi_unified_t wmi_handle,
 	 * to save total length calculated
 	 */
 	min_data_len =
-		(((uint64_t)ev->num_pdev_stats) * sizeof(wmi_pdev_stats)) +
+		(((uint64_t)ev->num_pdev_stats) * sizeof(wmi_pdev_stats_v2)) +
 		(((uint64_t)ev->num_vdev_stats) * sizeof(wmi_vdev_stats)) +
 		(((uint64_t)ev->num_peer_stats) * sizeof(wmi_peer_stats)) +
 		(((uint64_t)ev->num_bcnflt_stats) *
@@ -19333,7 +19333,8 @@ static QDF_STATUS extract_all_stats_counts_tlv(wmi_unified_t wmi_handle,
 /**
  * extract_pdev_tx_stats() - extract pdev tx stats from event
  */
-static void extract_pdev_tx_stats(wmi_host_dbg_tx_stats *tx, struct wlan_dbg_tx_stats *tx_stats)
+static void extract_pdev_tx_stats(wmi_host_dbg_tx_stats *tx,
+				  struct wlan_dbg_tx_stats_v2 *tx_stats)
 {
 	/* Tx Stats */
 	tx->comp_queued = tx_stats->comp_queued;
@@ -19366,7 +19367,8 @@ static void extract_pdev_tx_stats(wmi_host_dbg_tx_stats *tx, struct wlan_dbg_tx_
 /**
  * extract_pdev_rx_stats() - extract pdev rx stats from event
  */
-static void extract_pdev_rx_stats(wmi_host_dbg_rx_stats *rx, struct wlan_dbg_rx_stats *rx_stats)
+static void extract_pdev_rx_stats(wmi_host_dbg_rx_stats *rx,
+				  struct wlan_dbg_rx_stats_v2 *rx_stats)
 {
 	/* Rx Stats */
 	rx->mid_ppdu_route_change = rx_stats->mid_ppdu_route_change;
@@ -19410,8 +19412,8 @@ static QDF_STATUS extract_pdev_stats_tlv(wmi_unified_t wmi_handle,
 	data = param_buf->data;
 
 	if (index < ev_param->num_pdev_stats) {
-		wmi_pdev_stats *ev = (wmi_pdev_stats *) ((data) +
-				(index * sizeof(wmi_pdev_stats)));
+		wmi_pdev_stats_v2 *ev = (wmi_pdev_stats_v2 *) ((data) +
+				(index * sizeof(wmi_pdev_stats_v2)));
 
 		pdev_stats->chan_nf = ev->chan_nf;
 		pdev_stats->tx_frame_count = ev->tx_frame_count;
@@ -19510,7 +19512,7 @@ static QDF_STATUS extract_vdev_stats_tlv(wmi_unified_t wmi_handle,
 	if (index < ev_param->num_vdev_stats) {
 		wmi_vdev_stats *ev = (wmi_vdev_stats *) ((data) +
 				((ev_param->num_pdev_stats) *
-				sizeof(wmi_pdev_stats)) +
+				sizeof(wmi_pdev_stats_v2)) +
 				(index * sizeof(wmi_vdev_stats)));
 
 		vdev_stats->vdev_id = ev->vdev_id;
@@ -19562,7 +19564,8 @@ static QDF_STATUS extract_bcn_stats_tlv(wmi_unified_t wmi_handle,
 
 	if (index < ev_param->num_bcn_stats) {
 		wmi_bcn_stats *ev = (wmi_bcn_stats *) ((data) +
-			((ev_param->num_pdev_stats) * sizeof(wmi_pdev_stats)) +
+			((ev_param->num_pdev_stats) *
+			 sizeof(wmi_pdev_stats_v2)) +
 			((ev_param->num_vdev_stats) * sizeof(wmi_vdev_stats)) +
 			((ev_param->num_peer_stats) * sizeof(wmi_peer_stats)) +
 			((ev_param->num_chan_stats) * sizeof(wmi_chan_stats)) +
@@ -19599,7 +19602,8 @@ static QDF_STATUS extract_peer_stats_tlv(wmi_unified_t wmi_handle,
 
 	if (index < ev_param->num_peer_stats) {
 		wmi_peer_stats *ev = (wmi_peer_stats *) ((data) +
-			((ev_param->num_pdev_stats) * sizeof(wmi_pdev_stats)) +
+			((ev_param->num_pdev_stats) *
+			 sizeof(wmi_pdev_stats_v2)) +
 			((ev_param->num_vdev_stats) * sizeof(wmi_vdev_stats)) +
 			(index * sizeof(wmi_peer_stats)));
 
@@ -19669,7 +19673,8 @@ static QDF_STATUS extract_chan_stats_tlv(wmi_unified_t wmi_handle,
 
 	if (index < ev_param->num_chan_stats) {
 		wmi_chan_stats *ev = (wmi_chan_stats *) ((data) +
-			((ev_param->num_pdev_stats) * sizeof(wmi_pdev_stats)) +
+			((ev_param->num_pdev_stats) *
+			 sizeof(wmi_pdev_stats_v2)) +
 			((ev_param->num_vdev_stats) * sizeof(wmi_vdev_stats)) +
 			((ev_param->num_peer_stats) * sizeof(wmi_peer_stats)) +
 			(index * sizeof(wmi_chan_stats)));
