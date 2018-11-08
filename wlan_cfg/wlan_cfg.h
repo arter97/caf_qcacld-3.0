982602cc07ad90d63faef9e0ca563a986a7b7190
Pamidipati, Vijay <vpamidip@codeaurora.org>
2018-11-08 10:09:03 +0530

    qcacmn: Split Tx and Rx indication handling
    Add support to handle Tx completion processing and Rx processing into
    different NAPI threads/contexts for Lithium DP
    
    Change-Id: I4f5c6ef3ce969ce457e26cf3e8305df1ae782f09

--- wlan_cfg/wlan_cfg.h
+++ wlan_cfg/wlan_cfg.h
@@ -33,7 +33,8 @@
 #define DP_TX_NAPI_BUDGET_DIV_MASK 0
 #else
 #define MAX_PDEV_CNT 3
-#define WLAN_CFG_INT_NUM_CONTEXTS 7
+#define WLAN_CFG_INT_NUM_CONTEXTS 11
+#define WLAN_CFG_RXDMA1_ENABLE 1
 /*
  * This mask defines how many transmit frames account for 1 NAPI work unit
  * 0xFFFF means each 64K tx frame completions account for 1 unit of NAPI budget
@@ -63,8 +64,6 @@
 /* Miscellaneous configuration */
 #define MAX_IDLE_SCATTER_BUFS 16
 #define DP_MAX_IRQ_PER_CONTEXT 12
-#define DP_MAX_INTERRUPT_CONTEXTS 8
-#define DP_MAX_INTERRUPT_CONTEXTS 8
 #define MAX_HTT_METADATA_LEN 32
 #define MAX_NUM_PEER_ID_PER_PEER 8
 #define DP_MAX_TIDS 17
