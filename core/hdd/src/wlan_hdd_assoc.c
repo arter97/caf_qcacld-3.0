bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacld-3.0: Remove policy_mgr session in disassoc hdlr
In LFR2 roaming disassociation with old AP happens
immediately after preauth success. But policy_mgr
decrement this entry in association completion handler.
This results in policy_mgr assuming an extra connection
while checking hw mode. policy_mgr session for the
disassociated AP should be removed in disassoc success
response handler.

CRs-Fixed: 2316508
Change-Id: I41db1f5a365d3a510e27d10adcec2a7d765616d5

core/hdd/src/wlan_hdd_assoc.c
@@ -4902,6 +4902,23 @@ hdd_sme_roam_callback(void *pContext, struct csr_roam_info *roam_info,
 			wlan_hdd_sae_callback(adapter, roam_info);
 		break;
 
+	case eCSR_ROAM_ROAMING_START:
+		/*
+		 * For LFR2, Handle roaming start to remove disassociated
+		 * session
+		 */
+		if (roaming_offload_enabled(hdd_ctx))
+			break;
+		if (roamResult == eCSR_ROAM_RESULT_NOT_ASSOCIATED) {
+			hdd_debug("Decrement session of disassociated AP device_mode %d sessionId %d",
+				  adapter->device_mode,
+				  adapter->session_id);
+			policy_mgr_decr_session_set_pcl(hdd_ctx->psoc,
+							adapter->device_mode,
+							adapter->session_id);
+		}
+		break;
+
 	default:
 		break;
 	}

arter97/caf_qcacld-3.0

Confilicts:
	core/hdd/src/wlan_hdd_assoc.c
[Backport : member name mismatch, hdd_psoc mismatch psoc]

core/hdd/src/wlan_hdd_assoc.c
@@ -4825,6 +4825,23 @@ hdd_sme_roam_callback(void *pContext, struct csr_roam_info *roam_info,
 			wlan_hdd_sae_callback(adapter, roam_info);
 		break;
 
+	case eCSR_ROAM_ROAMING_START:
+		/*
+		 * For LFR2, Handle roaming start to remove disassociated
+		 * session
+		 */
+		if (roaming_offload_enabled(hdd_ctx))
+			break;
+		if (roamResult == eCSR_ROAM_RESULT_NOT_ASSOCIATED) {
+			hdd_debug("Decrement session of disassociated AP device_mode %d sessionId %d",
+				  adapter->device_mode,
+				  adapter->session_id);
+			policy_mgr_decr_session_set_pcl(hdd_ctx->hdd_psoc,
+							adapter->device_mode,
+							adapter->session_id);
+		}
+		break;
+
 	default:
 		break;
 	}