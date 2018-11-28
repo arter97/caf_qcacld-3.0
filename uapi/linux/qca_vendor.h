3420f5b7c8c9c4f6a2e14ecac000741cedaee962
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2018-11-28 19:40:17 +0530

    qcacld-3.0: Send sta auth status as per updated vendor attr
    Vendor attribute QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_STA_FLAGS
    is updated to accept the nla values in the form of
    nl80211_sta_flag_update structure instead of uint32_t flags at
    user space, the name of the vendor attribute
    QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_STA_MAC for the same event
    is changed to QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_MAC_ADDR and
    enum values are also updated for sta_flags and MAC_ADDR.
    
    Update the driver according to updated format of the event
    attributes.
    
    Change-Id: I54b95ecbe05af64c375a06bb36e59ae2b04ddf7f
    CRs-Fixed: 2359794

--- uapi/linux/qca_vendor.h
+++ uapi/linux/qca_vendor.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -2896,6 +2896,12 @@ enum qca_wlan_vendor_attr_get_logger_features {
  *	to specify negotiated rate flags i.e. ht, vht and channel width
  * @QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_FREQ: Unsigned 32bit value to
  *	specify the operating frequency
+ * @QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_MAC_ADDR: MAC Address of the peer
+ * (STA / AP ) for the connected link.
+ * @QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_STA_FLAGS: Attribute containing a
+ * &struct nl80211_sta_flag_update for the respective connected link. MAC
+ * address of the peer represented by
+ * QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_MAC_ADDR.
  * @QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_AFTER_LAST: after last
  * @QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_MAX: max value
  */
@@ -2904,8 +2910,10 @@ enum qca_wlan_vendor_attr_link_properties {
 	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_NSS = 1,
 	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_RATE_FLAGS = 2,
 	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_FREQ = 3,
-	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_STA_FLAGS  = 4,
-	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_STA_MAC  = 5,
+	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_MAC_ADDR = 4,
+	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_STA_FLAGS = 5,
+
+
 
 	/* KEEP LAST */
 	QCA_WLAN_VENDOR_ATTR_LINK_PROPERTIES_AFTER_LAST,
