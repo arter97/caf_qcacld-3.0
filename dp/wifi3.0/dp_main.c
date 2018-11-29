5cda853b54173faa0d666caff13f9848ab536c4f
Vinay Adella <vadella@codeaurora.org>
2018-11-29 20:04:29 +0530

    qcacmn: Change peer unref delete API parameters
    Pass vdev mac-addr and vdev mode, instead of vdev id.
    
    Change-Id: I2148e7ecef433f4363385eeaec2e18f3ec131ab8

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -3774,7 +3774,8 @@ static void *dp_peer_create_wifi3(struct cdp_vdev *vdev_handle,
 		*/
 		if (soc->cdp_soc.ol_ops->peer_unref_delete) {
 			soc->cdp_soc.ol_ops->peer_unref_delete(pdev->osif_pdev,
-				vdev->vdev_id, peer->mac_addr.raw);
+				peer->mac_addr.raw, vdev->mac_addr.raw,
+				vdev->opmode);
 		}
 
 		DP_STATS_INIT(peer);
@@ -4499,7 +4500,8 @@ void dp_peer_unref_delete(void *peer_handle)
 
 		if (soc->cdp_soc.ol_ops->peer_unref_delete) {
 			soc->cdp_soc.ol_ops->peer_unref_delete(pdev->osif_pdev,
-					vdev_id, peer->mac_addr.raw);
+					peer->mac_addr.raw, vdev->mac_addr.raw,
+					vdev->opmode);
 		}
 
 		if (!vdev || !vdev->vap_bss_peer) {
