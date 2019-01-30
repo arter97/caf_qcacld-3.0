d2325f0174523509e22fa0259e2c34ba4f01dd40
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2019-01-30 16:58:04 +0530

    qcacld-3.0: Send all assoc request IE to hostapd in SAP mode
    Host driver packs only the RSN IE, WAP IE and additional IE
    from association request to upper layers. With any new IE
    support added on the driver, the driver has to add new structure
    to csr_roam_info and pack that IE into the
    tSap_StationAssocReassocCompleteEvent and pass it to higher
    layers. This increases the redundancy with new IE introduced.
    
    To facilitate hostapd to get information on all IE, pass all the
    IE present in the assoc request to hostapd.
    
    Change-Id: I1665a83de52bd67529ea026dc0c5e92cfbd51590
    CRs-Fixed: 2390425

--- core/sme/inc/csr_api.h
+++ core/sme/inc/csr_api.h
@@ -1550,6 +1550,8 @@ typedef struct sSirSmeAssocIndToUpperLayerCnf {
 	/* Extended capabilities of STA */
 	uint8_t              ecsa_capable;
 
+	uint32_t ies_len;
+	uint8_t *ies;
 	tDot11fIEHTCaps ht_caps;
 	tDot11fIEVHTCaps vht_caps;
 } tSirSmeAssocIndToUpperLayerCnf, *tpSirSmeAssocIndToUpperLayerCnf;
