d80104119318bb3eedc72d890b8399ca964c4914
Varsha Mishra <varsham@codeaurora.org>
2019-05-13 11:55:22 +0530

    qcacmn: Fill skb->priority only for macro ATH_RX_PRI_SAVE
    skb->priority is needed only for ATH_RX_PRI_SAVE. This access can
    be avoided in regular datapath by using skb->cb for faster access.
    
    Change-Id: I5ef970fa45fa314db7e14284d898c98a59d8591e

--- dp/wifi3.0/dp_rx.c
+++ dp/wifi3.0/dp_rx.c
@@ -30,6 +30,13 @@
 #include "dp_rx_mon.h"
 #include "dp_ipa.h"
 
+#ifdef ATH_RX_PRI_SAVE
+#define DP_RX_TID_SAVE(_nbuf, _tid) \
+	(qdf_nbuf_set_priority(_nbuf, _tid))
+#else
+#define DP_RX_TID_SAVE(_nbuf, _tid)
+#endif
+
 #ifdef CONFIG_WIN
 static inline bool dp_rx_check_ap_bridge(struct dp_vdev *vdev)
 {
@@ -42,23 +49,6 @@ static inline bool dp_rx_check_ap_bridge(struct dp_vdev *vdev)
 		return true;
 	else
 		return false;
-}
-#endif
-
-#ifdef ATH_RX_PRI_SAVE
-static inline void dp_rx_save_tid_ts(qdf_nbuf_t nbuf, uint8_t tid, bool flag)
-{
-	qdf_nbuf_set_priority(nbuf, tid);
-	if (qdf_unlikely(flag))
-		qdf_nbuf_set_timestamp(nbuf);
-}
-#else
-static inline void dp_rx_save_tid_ts(qdf_nbuf_t nbuf, uint8_t tid, bool flag)
-{
-	if (qdf_unlikely(flag)) {
-		qdf_nbuf_set_priority(nbuf, tid);
-		qdf_nbuf_set_timestamp(nbuf);
-	}
 }
 #endif
 
@@ -1907,7 +1897,10 @@ done:
 		 * to be written
 		 */
 		rx_pdev = soc->pdev_list[rx_desc->pool_id];
-		dp_rx_save_tid_ts(nbuf, tid, rx_pdev->delay_stats_flag);
+		DP_RX_TID_SAVE(nbuf, tid);
+		if (qdf_unlikely(rx_pdev->delay_stats_flag))
+			qdf_nbuf_set_timestamp(nbuf);
+
 		tid_stats = &rx_pdev->stats.tid_stats.tid_rx_stats[tid];
 		if (qdf_unlikely(!dp_rx_is_msdu_done_set(soc, rx_tlv_hdr))) {
 			tid_stats->fail_cnt[MSDU_DONE_FAILURE]++;
