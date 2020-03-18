bittencourtjulival/eclipse_kernel_xiaomi_stone

qcacmn: Fix issue on block ACK on response to BAR
Fix the issue on block ACK on response to BAR frame.
Parse BAR frame received from monitor destination ring and
generate block ack frame from BAR frame and previous block
ACK frame.

Change-Id: Ifc242b3850630aa25827b003a1e2078a9228358a

hal/wifi3.0/hal_api_mon.h
@@ -224,6 +224,25 @@ bool HAL_RX_HW_DESC_MPDU_VALID(void *hw_desc_addr)
 	return tlv_tag == WIFIRX_MPDU_START_E ? true : false;
 }
 
+/*
+ * HAL_RX_HW_DESC_MPDU_VALID() - check MPDU start TLV user id in MPDU
+ *			start TLV of Hardware TLV descriptor
+ * @hw_desc_addr: Hardware desciptor address
+ *
+ * Return: unit32_t: user id
+ */
+static inline
+uint32_t HAL_RX_HW_DESC_MPDU_USER_ID(void *hw_desc_addr)
+{
+	struct rx_mon_pkt_tlvs *rx_desc =
+		(struct rx_mon_pkt_tlvs *)hw_desc_addr;
+	uint32_t user_id;
+
+	user_id = HAL_RX_GET_USER_TLV32_USERID(
+		&rx_desc->mpdu_start_tlv);
+
+	return user_id;
+}
 
 /* TODO: Move all Rx descriptor functions to hal_rx.h to avoid duplication */
 
@@ -544,12 +563,12 @@ struct mon_rx_info {
 	uint16_t qos_control;
 	uint8_t mac_addr1_valid;
 	uint8_t mac_addr1[QDF_MAC_ADDR_SIZE];
+	uint32_t user_id;
 };
 
 struct mon_rx_user_info {
 	uint16_t qos_control;
 	uint8_t qos_control_info_valid;
-	uint32_t bar_frame:1;
 };
 
 struct hal_rx_ppdu_info {

arter97/caf_qcacld-3.0

hal/wifi3.0/hal_api_mon.h
@@ -240,6 +240,27 @@ uint32_t HAL_RX_HW_DESC_GET_PPDUID_GET(void *hw_desc_addr)
 	return HAL_RX_GET(rx_mpdu_info, RX_MPDU_INFO_0, PHY_PPDU_ID);
 }
 
+/*
+ * HAL_RX_HW_DESC_MPDU_VALID() - check MPDU start TLV user id in MPDU
+ *			start TLV of Hardware TLV descriptor
+ * @hw_desc_addr: Hardware desciptor address
+ *
+ * Return: unit32_t: user id
+ */
+static inline
+uint32_t HAL_RX_HW_DESC_MPDU_USER_ID(void *hw_desc_addr)
+{
+	struct rx_pkt_tlvs *rx_desc =
+		(struct rx_pkt_tlvs *)hw_desc_addr;
+
+	uint32_t user_id;
+
+	user_id = HAL_RX_GET_USER_TLV32_USERID(
+		&rx_desc->mpdu_start_tlv);
+
+	return user_id;
+}
+
 /* TODO: Move all Rx descriptor functions to hal_rx.h to avoid duplication */
 
 #define HAL_RX_BUFFER_ADDR_31_0_GET(buff_addr_info)		\
@@ -485,12 +506,12 @@ struct mon_rx_info {
 	uint16_t qos_control;
 	uint8_t mac_addr1_valid;
 	uint8_t mac_addr1[QDF_MAC_ADDR_SIZE];
+	uint32_t user_id;
 };
 
 struct mon_rx_user_info {
 	uint16_t qos_control;
 	uint8_t qos_control_info_valid;
-	uint32_t bar_frame:1;
 };
 
 struct hal_rx_ppdu_info {