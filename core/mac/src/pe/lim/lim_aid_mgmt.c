951d7294f16f75b31fc43cc49a4cff8ad62c4e73
hqu <hqu@codeaurora.org>
2020-05-01 00:09:32 +0800

    qcacld-3.0: Add maximum sta number limitation for each pe_session
    Currently use maxStation to create dph_hash_table for each
    pe_session, actually maxStation is total allowed maximum
    sta for all pe_session that gets from ini gSoftApMaxPeers
    and firmware capability.
    
    SIR_SAP_MAX_NUM_PEERS is maximum sta number for each pe_session
    and can be configured from config file, add max_sta_of_pe_session
    to limit maximum sta number for each pe_session, set
    max_sta_of_pe_session as minimum value between maxStation and
    SIR_SAP_MAX_NUM_PEERS.
    
    Change-Id: Iaa01c91cd164c470b3f76bffc826ca5abdd4d0d3
    CRs-Fixed: 2676484

--- core/mac/src/pe/lim/lim_aid_mgmt.c
+++ core/mac/src/pe/lim/lim_aid_mgmt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2016, 2018-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2016, 2018-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -55,7 +55,7 @@
 void lim_init_peer_idxpool(struct mac_context *mac, struct pe_session *pe_session)
 {
 	uint8_t i;
-	uint8_t maxAssocSta = mac->lim.maxStation;
+	uint8_t maxAssocSta = mac->lim.max_sta_of_pe_session;
 
 	pe_session->gpLimPeerIdxpool[0] = 0;
 
