bebc7db9befd509503b125c00d8cedf34b961dbc
Amir <amirp@codeaurora.org>
2020-11-16 21:37:14 +0530

    qcacmn: Disable rxdma1 functionality for LM256 profile
    As monitor mode is not supported on LM256 profile,
    disable rxdma1 functionality on same
    
    Change-Id: I64e5de02d56801bfd46e32a6c64a1286e4d7ecb0
    CRs-Fixed: 2819904

--- wlan_cfg/wlan_cfg.c
+++ wlan_cfg/wlan_cfg.c
@@ -1481,3 +1481,15 @@ uint8_t wlan_cfg_radio2_default_reo_get(struct wlan_cfg_dp_soc_ctxt *cfg)
 {
 	return cfg->radio2_rx_default_reo;
 }
+
+#ifdef QCA_LOWMEM_CONFIG
+void wlan_cfg_set_rxdma1_enable(struct wlan_cfg_dp_soc_ctxt *cfg)
+{
+	cfg->rxdma1_enable = false;
+}
+#else
+void wlan_cfg_set_rxdma1_enable(struct wlan_cfg_dp_soc_ctxt *cfg)
+{
+	cfg->rxdma1_enable = true;
+}
+#endif
