506685df6d2e9bee2c3c96b991a922bcc00ccd8a
Srinivas Dasari <dasaris@codeaurora.org>
2020-12-23 09:59:43 +0530

    qcacld-3.0: Send assoc reject upon failing to post ASSOC_IND
    Currently, lim silently drops the association if it fails to
    post ASSOC_IND due to some reason(e.g. invalid contents of
    assoc request) and the MLM state is stuck in
    eLIM_MLM_WT_ASSOC_CNF_STATE. Station context is not cleaned up
    till the next association. Gracefully cleanup the association
    in such failure cases.
    
    CRs-Fixed: 2810235
    (cherry picked from commit d390f1e5a2551f12745474477bbf7daf2364d65f)
    
    Change-Id: I71bf41d3e72fc391adf7089bb2a16014722bf968

--- core/mac/src/pe/lim/lim_process_assoc_req_frame.c
+++ core/mac/src/pe/lim/lim_process_assoc_req_frame.c
@@ -2965,7 +2965,7 @@ bool lim_fill_lim_assoc_ind_params(
  *
  * Return: None
  */
-void lim_send_mlm_assoc_ind(struct mac_context *mac_ctx,
+QDF_STATUS lim_send_mlm_assoc_ind(struct mac_context *mac_ctx,
 	tpDphHashNode sta_ds, struct pe_session *session_entry)
 {
 	tpLimMlmAssocInd assoc_ind;
@@ -2976,7 +2976,7 @@ void lim_send_mlm_assoc_ind(struct mac_context *mac_ctx,
 
 	if (!session_entry->parsedAssocReq) {
 		pe_err(" Parsed Assoc req is NULL");
-		return;
+		return QDF_STATUS_E_INVAL;
 	}
 
 	/* Get a copy of the already parsed Assoc Request */
@@ -2985,7 +2985,7 @@ void lim_send_mlm_assoc_ind(struct mac_context *mac_ctx,
 
 	if (!assoc_req) {
 		pe_err("assoc req for assoc_id:%d is NULL", sta_ds->assocId);
-		return;
+		return QDF_STATUS_E_INVAL;
 	}
 
 	/* Get the phy_mode */
@@ -3008,18 +3008,18 @@ void lim_send_mlm_assoc_ind(struct mac_context *mac_ctx,
 		assoc_ind = qdf_mem_malloc(temp);
 		if (!assoc_ind) {
 			lim_release_peer_idx(mac_ctx, sta_ds->assocId,
-					     session_entry);
-			return;
+				session_entry);
+			return QDF_STATUS_E_NOMEM;
 		}
 		if (!lim_fill_lim_assoc_ind_params(assoc_ind, mac_ctx,
 						   sta_ds, session_entry)) {
 			qdf_mem_free(assoc_ind);
-			return;
+			return QDF_STATUS_E_INVAL;
 		}
 		lim_post_sme_message(mac_ctx, LIM_MLM_ASSOC_IND,
 				     (uint32_t *)assoc_ind);
 		qdf_mem_free(assoc_ind);
 	}
 
-	return;
+	return QDF_STATUS_SUCCESS;
 }
