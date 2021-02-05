70f48593fb5a165053fb4f96a5072c67b09db41e
Shikha Bakshi <bakshi@codeaurora.org>
2021-02-05 12:52:26 +0530

    qcacmn: Add peer_unauth_rx_drop stats member
    Add the stats-member to count the numer pf packets
    dropped when peer is unauthorized.
    
    Change-Id: I7d73f55541b0be4b172893221bb2c7ac8cf7b72e

--- dp/inc/cdp_txrx_stats_struct.h
+++ dp/inc/cdp_txrx_stats_struct.h
@@ -1957,6 +1957,8 @@ struct cdp_pdev_stats {
 		uint64_t num_pool_bufs_replenish;
 		uint64_t num_bufs_alloc_success;
 	} rx_buffer_pool;
+
+	uint32_t peer_unauth_rx_pkt_drop;
 };
 
 /* struct cdp_peer_hmwds_ast_add_status - hmwds peer ast add status
