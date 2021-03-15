b305e4bca92f6b512f2d1c348c6a7450c8710626
Varsha Mishra <varsham@codeaurora.org>
2021-03-15 17:22:18 +0530

    qcacmn: Enable peer authorize by default
    Drop non-EAPOL frames from unauthorized peer in security mode.
    Enabling this feature by default with this change.
    
    Change-Id: I9621d6ecd65d996aee8f1455255569e426f566f3

--- dp/wifi3.0/dp_types.h
+++ dp/wifi3.0/dp_types.h
@@ -789,6 +789,8 @@ struct dp_soc_stats {
 			uint32_t scatter_msdu;
 			/* RX msdu drop count due to invalid cookie */
 			uint32_t invalid_cookie;
+			/* Non Eapol packet drop count due to peer not authorized  */
+			uint32_t peer_unauth_rx_pkt_drop;
 		} err;
 
 		/* packet count per core - per ring */
