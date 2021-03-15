0275556da72120e31ece2c308f568b7ce080bd88
Varsha Mishra <varsham@codeaurora.org>
2021-03-15 17:29:25 +0530

    qcacmn: Add peer_unauth_rx_drop stat in cdp_pdev_stats
    Add the stat to count the numer pf packets dropped
    when peer is unauthorized.
    
    Change-Id: I7d73f55541b0be4b172893221bb2c7ac8cf7b72e

--- dp/inc/cdp_txrx_stats_struct.h
+++ dp/inc/cdp_txrx_stats_struct.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2021 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -1733,6 +1733,7 @@ struct cdp_pdev_stats {
 	uint64_t tx_ppdu_proc;
 	uint32_t ack_ba_comes_twice;
 	uint32_t ppdu_drop;
+	uint32_t peer_unauth_rx_pkt_drop;
 };
 
 #ifdef QCA_ENH_V3_STATS_SUPPORT
