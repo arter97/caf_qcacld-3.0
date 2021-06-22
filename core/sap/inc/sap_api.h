5c08b8c614bc8490243351eaa80d1d7f576c6f42
Surya Prakash Sivaraj <suryapra@codeaurora.org>
2021-06-22 10:40:01 +0530

    qcacld-3.0: Save RSN auth type for RSN association
    Save RSN auth type in related context for RSN assocoation.
    Some OWE specific operation need to be done according to
    this RSN auth type since we cannot tell if a STA / SAP is OWE,
    WPA2 or OPEN by its auth type OPEN.
    
    Change-Id: I17dd72c7adf1c663725ac58ce263f4408d9c967b
    CRs-Fixed: 2396885

--- core/sap/inc/sap_api.h
+++ core/sap/inc/sap_api.h
@@ -608,6 +608,7 @@ typedef struct sap_config {
 	uint8_t RSNEncryptType;
 	uint8_t mcRSNEncryptType;
 	eSapAuthType authType;
+	tCsrAuthList akm_list;
 	bool privacy;
 	bool UapsdEnable;
 	bool fwdWPSPBCProbeReq;
