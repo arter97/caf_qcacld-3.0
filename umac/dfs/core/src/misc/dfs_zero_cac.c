52ebce53d3b27f86971ebb8c204f470498153a08
Vignesh U <uvignesh@codeaurora.org>
2021-07-21 12:02:32 +0530

    qca-wifi: Check current DFS domain to find if ADFS is enabled
    The Agile precac enabled check does not consider the DFS domain check.
    
    Consider a case, where the ADFS was enabled for an ETSI country and the
    ADFS picks a channel and runs precac. When a country change command is
    issued by the user say "US" country, the ADFS is restarted despite the
    new country being a non-ETSI country. This is because the precac
    enabled check does not consider the DFS domain check.
    
    Add DFS domain check to precac enabled check to prevent such scenarios.
    
    Change-Id: I3c07489c9283d7542e6c6c86bb6d1adc64e77df1
    CRs-Fixed: 2994630

--- umac/dfs/core/src/misc/dfs_zero_cac.c
+++ umac/dfs/core/src/misc/dfs_zero_cac.c
@@ -422,6 +422,11 @@ void dfs_zero_cac_timer_detach(struct dfs_soc_priv_obj *dfs_soc_obj)
 #ifdef QCA_SUPPORT_AGILE_DFS
 bool dfs_is_agile_precac_enabled(struct wlan_dfs *dfs)
 {
+	enum dfs_reg dfsdomain = utils_get_dfsdomain(dfs->dfs_pdev_obj);
+
+	if (dfsdomain != DFS_ETSI_REGION)
+		return false;
+
 	return (dfs->dfs_agile_precac_ucfg && dfs->dfs_fw_adfs_support_non_160);
 }
 #endif
