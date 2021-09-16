b22d442f7d2930c7bd9fe7b9d4975f18c2a9a91e
Bing Sun <quic_bings@quicinc.com>
2021-09-16 14:04:52 +0800

    qcacld-3.0: Set/get candidate channel to switch when radar is detected
    Currently driver switches to a random channel when radar is detected.
    Easymesh expects ap switching to a given channel.
    
    Add API to set/get given channel. AP should switch to given channel
    when radar is detected.
    The given channel will be cleared after a radar detection.
    
    Change-Id: I29b97e247583be768842c128e6a3e5d5bfd859f7
    CRs-Fixed: 3034735

--- core/sap/src/sap_internal.h
+++ core/sap/src/sap_internal.h
@@ -243,6 +243,7 @@ struct sap_context {
  */
 	bool is_forcescc_restart_required;
 #endif
+	qdf_freq_t candidate_freq;
 };
 
 /*----------------------------------------------------------------------------
