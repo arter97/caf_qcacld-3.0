4af6ad17740622003b95a3c79eb353c4d50a9632
Madan Koyyalamudi <mkoyya@codeaurora.org>
2021-11-04 21:16:13 -0700

    Release 5.1.1.79
    Release 5.1.1.79
    
    Change-Id: I8754bd76e02f7e6848c4b36769f602eb42fac3a8
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            78
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            79
 
-#define QWLAN_VERSIONSTR               "5.1.1.78Z"
+#define QWLAN_VERSIONSTR               "5.1.1.79"
 
 #endif /* QWLAN_VERSION_H */
