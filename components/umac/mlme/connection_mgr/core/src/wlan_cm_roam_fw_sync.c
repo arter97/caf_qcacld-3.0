1513172813ff3d18cf0cd0ed34ac432e418b834c
Jianmin Zhu <jianminz@codeaurora.org>
2021-11-17 15:18:32 +0800

    qcacld-3.0: Fix roam failed for SAP CSA concurrency
    When vdev0 STA roamed to another channel, vdev1 SAP CSA for SCC
    when ROAM_SYNCH_IN_PROG, will disable roam, but no RSO stop to F/W,
    F/W RSO isn't disabled, but host vdev0 will enter RSO_STOPPED, host
    F/W RSO state out of sync.
    If vdev0 STA next roam sync come soon,  it won't be handled for host
    RSO_STOPPED, neither roam sync complete nor disconnect will happen,
    F/W will asert after wait roam sync complete timeout.
    
    To fix it, Only do SAP CSA check after roam complete and RSO
    reenabled, can avoid host F/W RSO state out of sync issue.
    
    Change-Id: I77ccf30cac38f62c6ef8330c6da00364249b242e
    CRs-Fixed: 3076935

--- components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_fw_sync.c
+++ components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_fw_sync.c
@@ -933,7 +933,6 @@ QDF_STATUS cm_fw_roam_complete(struct cnx_mgr *cm_ctx, void *data)
 	}
 	policy_mgr_check_n_start_opportunistic_timer(psoc);
 
-	policy_mgr_check_concurrent_intf_and_restart_sap(psoc);
 	if (roam_synch_data->auth_status == ROAM_AUTH_STATUS_AUTHENTICATED)
 		wlan_cm_roam_state_change(pdev, vdev_id,
 					  WLAN_ROAM_RSO_ENABLED,
@@ -954,6 +953,7 @@ QDF_STATUS cm_fw_roam_complete(struct cnx_mgr *cm_ctx, void *data)
 		wlan_cm_roam_state_change(pdev, vdev_id,
 					  WLAN_ROAM_RSO_STOPPED,
 					  REASON_DISCONNECTED);
+	policy_mgr_check_concurrent_intf_and_restart_sap(psoc);
 end:
 	return status;
 }
