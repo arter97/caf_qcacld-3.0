ef0b6e5ada16dc8abe3f60c11885d0c319d5edd7
Will Huang <quic_wilhuang@quicinc.com>
2021-12-17 13:04:10 +0800

    qcacld-3.0: Add static cfg items and APIs for AFC feature
    Add 4 static cfg items for AFC feature:
    CFG_AFC_REG_NO_ACTION, CFG_6GHZ_SP_POWER_MODE_SUPP,
    CFG_AFC_TIMER_CHECK_DIS, CFG_AFC_REQ_ID_CHECK_DIS.
    
    Add APIs to pass these settings to psoc private of regulatory
    component.
    
    Change-Id: I61114fb7bf7d84f939265629bde78813b6295e39
    CRs-Fixed: 3153987

--- components/mlme/dispatcher/inc/wlan_mlme_ucfg_api.h
+++ components/mlme/dispatcher/inc/wlan_mlme_ucfg_api.h
@@ -4378,4 +4378,75 @@ ucfg_mlme_cfg_get_ht_smps(struct wlan_objmgr_psoc *psoc,
 {
 	return wlan_mlme_cfg_get_ht_smps(psoc, value);
 }
+
+#if defined(CONFIG_AFC_SUPPORT) && defined(CONFIG_BAND_6GHZ)
+/**
+ * ucfg_mlme_get_enable_6ghz_sp_mode_support() - Get 6 GHz SP mode support cfg
+ * @psoc: pointer to psoc object
+ * @value: value to be set
+ *
+ * Return: QDF Status
+ */
+QDF_STATUS
+ucfg_mlme_get_enable_6ghz_sp_mode_support(struct wlan_objmgr_psoc *psoc,
+					  bool *value);
+
+/**
+ * ucfg_mlme_get_afc_disable_timer_check() - Get AFC timer check cfg
+ * @psoc: pointer to psoc object
+ * @value: value to be set
+ *
+ * Return: QDF Status
+ */
+QDF_STATUS
+ucfg_mlme_get_afc_disable_timer_check(struct wlan_objmgr_psoc *psoc,
+				      bool *value);
+/**
+ * ucfg_mlme_get_afc_disable_request_id_check() - Get AFC request id check cfg
+ * @psoc: pointer to psoc object
+ * @value: value to be set
+ *
+ * Return: QDF Status
+ */
+QDF_STATUS
+ucfg_mlme_get_afc_disable_request_id_check(struct wlan_objmgr_psoc *psoc,
+					   bool *value);
+
+/**
+ * ucfg_mlme_get_afc_reg_noaction() - Get AFC no action cfg
+ * @psoc: pointer to psoc object
+ * @value: value to be set
+ *
+ * Return: QDF Status
+ */
+QDF_STATUS
+ucfg_mlme_get_afc_reg_noaction(struct wlan_objmgr_psoc *psoc, bool *value);
+#else
+static inline QDF_STATUS
+ucfg_mlme_get_enable_6ghz_sp_mode_support(struct wlan_objmgr_psoc *psoc,
+					  bool *value)
+{
+	return QDF_STATUS_E_NOSUPPORT;
+}
+
+static inline QDF_STATUS
+ucfg_mlme_get_afc_disable_timer_check(struct wlan_objmgr_psoc *psoc,
+				      bool *value)
+{
+	return QDF_STATUS_E_NOSUPPORT;
+}
+
+static inline QDF_STATUS
+ucfg_mlme_get_afc_disable_request_id_check(struct wlan_objmgr_psoc *psoc,
+					   bool *value)
+{
+	return QDF_STATUS_E_NOSUPPORT;
+}
+
+static inline QDF_STATUS
+ucfg_mlme_get_afc_reg_noaction(struct wlan_objmgr_psoc *psoc, bool *value)
+{
+	return QDF_STATUS_E_NOSUPPORT;
+}
+#endif
 #endif /* _WLAN_MLME_UCFG_API_H_ */
