99049dd7f827669634ee98200704367cbbfe7c8c
Vignesh Mohan <quic_vignmoha@quicinc.com>
2022-02-15 17:48:25 +0530

    qca-wifi: Use the n_valid subchannel count in preCAC done check
    While bringing up an AP in channel 100 HT20, even is preCAC is disabled,
    channel 100 HT20 is marked as preCAC done and CAC is skipped.
    
    While checking if preCAC is done for a particular
    frequency, the tree nodes are traversed considering each node
    has subchannel count in powers of 2. While traversing from HT160 (root)
    to HT20, the subchannel count is 8->4->2->1. When we reach
    100HT20, the number of subchannels is 1 and only if
    CACed subchannels count is also 1, 100HT20 is considered preCAC done.
    With the addition of 240MHz, the subchannel count is no longer
    in powers of 2. While traversing from 240MHz, the subchannel count,
    if considered in powers of 2, is 12->6->3->1->0 (instead of
    12->8->4->2->1). While checking for 100HT20, the number of
    subchannels is 0 and since CAC subchannels count is also 0, 100HT20
    is always considered CAC done.
    
    While checking if preCAC is done for a particular center frequency,
    use the n_valid subchannel count for every child node traversed
    by the API, instead of dividing the initial n_valid subchannel
    count by 2.

--- umac/dfs/core/src/misc/dfs_precac_list.c
+++ umac/dfs/core/src/misc/dfs_precac_list.c
@@ -1028,14 +1028,12 @@ dfs_find_cac_status_for_chan_for_freq(struct dfs_precac_entry *precac_entry,
 				      uint16_t chan_freq)
 {
 	struct precac_tree_node *node = precac_entry->tree_root;
-	uint8_t n_cur_lvl_subchs = node->n_valid_subchs;
 
 	while (node) {
 		if (node->ch_freq == chan_freq)
-			return (node->n_caced_subchs == n_cur_lvl_subchs) ?
+			return (node->n_caced_subchs == node->n_valid_subchs) ?
 				true : false;
 
-		n_cur_lvl_subchs /= 2;
 		node = dfs_descend_precac_tree_for_freq(node, chan_freq);
 	}
 
