6fde15fafb709334768a05f629bc0daad4cd75b6
Vivek <quic_vchettri@quicinc.com>
2022-03-03 03:02:38 +0530

    qca-wifi: Add stub wifi if API for NSS and SAWF
    The NSS driver call the wifi driver to get queue related
    information matching the flow. The wifi driver exports
    an API to achieve this.
    
    And for SAWF disabled cases this should return invalid queue ID
    
    Change-Id: I43a45ee7ab5043057b8066211180f5bb48026ccd
    CRs-Fixed: 3142629

--- qca_sawf/qca_sawf.c
+++ qca_sawf/qca_sawf.c
@@ -13,7 +13,9 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#include<dp_sawf.h>
+
+#ifdef CONFIG_SAWF
+#include <dp_sawf.h>
 #include <wlan_sawf.h>
 
 uint16_t qca_sawf_get_msduq(struct net_device *netdev, uint8_t *peer_mac,
@@ -28,5 +30,15 @@ uint16_t qca_sawf_get_msduq(struct net_device *netdev, uint8_t *peer_mac,
 
 	return dp_sawf_get_msduq(netdev, peer_mac, service_id);
 }
+#else
+
+#include "qdf_module.h"
+#define DP_SAWF_PEER_Q_INVALID 0xffff
+uint16_t qca_sawf_get_msduq(struct net_device *netdev, uint8_t *peer_mac,
+			    uint32_t service_id)
+{
+	return DP_SAWF_PEER_Q_INVALID;
+}
+#endif
 
 qdf_export_symbol(qca_sawf_get_msduq);
