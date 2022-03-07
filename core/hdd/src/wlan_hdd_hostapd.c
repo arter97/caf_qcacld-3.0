sonyxperiadev/vendor-qcom-opensource-wlan-qcacld-3.0

qcacld-3.0: Fix 3rd SAP failed to avoid LTE unsafe channel
wlan0 on 5180, wlan2 on 5180, wlan1 on 2462. wlan1 conf file
has ACS “freqlist=2457-2472,5180-5945”.
When LTE unsafe event comes, it marks 2457, 2462, 5180 unsafe,
wlan0 changes channel to 5200. wlan1 has no available 2G channels.
It selects 5G channel 5200. But 5G has two vdevs already, then
Concurrency check failed - drv doesn’t support 3 home channel.
wlan1 keep active on 2457, but driver doesn't continue to check
wlan2's channel and do CSA to safe channel.
Fix by checking the wlan1's CSA status and if status is failure
then do unsafe channel check for 3rd wlan2.

Change-Id: I28e9397436780cf47e57a47482b729a42fb728f0
CRs-Fixed: 3136773

core/hdd/src/wlan_hdd_hostapd.c
@@ -3462,23 +3462,11 @@ void hdd_stop_sap_set_tx_power(struct wlan_objmgr_psoc *psoc,
 }
 
 #ifdef FEATURE_WLAN_MCC_TO_SCC_SWITCH
-/**
- * hdd_sap_restart_with_channel_switch() - SAP channel change with E/CSA
- * @wlan_objmgr_psoc: psoc common object
- * @ap_adapter: HDD adapter
- * @target_channel: Channel to which switch must happen
- * @target_bw: Bandwidth of the target channel
- * @forced: Force to switch channel, ignore SCC/MCC check
- *
- * Invokes the necessary API to perform channel switch for the SAP or GO
- *
- * Return: None
- */
-void hdd_sap_restart_with_channel_switch(struct wlan_objmgr_psoc *psoc,
-					 struct hdd_adapter *ap_adapter,
-					 uint32_t target_chan_freq,
-					 uint32_t target_bw,
-					 bool forced)
+QDF_STATUS hdd_sap_restart_with_channel_switch(struct wlan_objmgr_psoc *psoc,
+					       struct hdd_adapter *ap_adapter,
+					       uint32_t target_chan_freq,
+					       uint32_t target_bw,
+					       bool forced)
 {
 	struct net_device *dev = ap_adapter->dev;
 	int ret;
@@ -3487,7 +3475,7 @@ void hdd_sap_restart_with_channel_switch(struct wlan_objmgr_psoc *psoc,
 
 	if (!dev) {
 		hdd_err("Invalid dev pointer");
-		return;
+		return QDF_STATUS_E_INVAL;
 	}
 
 	ret = hdd_softap_set_channel_change(dev, target_chan_freq,
@@ -3495,25 +3483,27 @@ void hdd_sap_restart_with_channel_switch(struct wlan_objmgr_psoc *psoc,
 	if (ret) {
 		hdd_err("channel switch failed");
 		hdd_stop_sap_set_tx_power(psoc, ap_adapter);
-		return;
 	}
+
+	return qdf_status_from_os_return(ret);
 }
 
-void hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
-				    uint8_t vdev_id, uint32_t ch_freq,
-				    uint32_t channel_bw,
-				    bool forced)
+QDF_STATUS hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
+					  uint8_t vdev_id, uint32_t ch_freq,
+					  uint32_t channel_bw, bool forced)
 {
 	struct hdd_adapter *ap_adapter =
 		wlan_hdd_get_adapter_from_vdev(psoc, vdev_id);
 
 	if (!ap_adapter) {
 		hdd_err("Adapter is NULL");
-		return;
+		return QDF_STATUS_E_INVAL;
 	}
-	hdd_sap_restart_with_channel_switch(psoc, ap_adapter,
-					    ch_freq,
-					    channel_bw, forced);
+
+	return hdd_sap_restart_with_channel_switch(psoc,
+						   ap_adapter,
+						   ch_freq,
+						   channel_bw, forced);
 }
 
 void wlan_hdd_set_sap_csa_reason(struct wlan_objmgr_psoc *psoc, uint8_t vdev_id,
@@ -3546,6 +3536,7 @@ QDF_STATUS wlan_hdd_get_channel_for_sap_restart(
 	struct sap_context *sap_context;
 	enum sap_csa_reason_code csa_reason =
 		CSA_REASON_CONCURRENT_STA_CHANGED_CHANNEL;
+	QDF_STATUS status;
 
 	if (!ap_adapter) {
 		hdd_err("ap_adapter is NULL");
@@ -3668,11 +3659,11 @@ sap_restart:
 	hdd_ap_ctx->bss_stop_reason = BSS_STOP_DUE_TO_MCC_SCC_SWITCH;
 	*ch_freq = intf_ch_freq;
 	hdd_debug("SAP channel change with CSA/ECSA");
-	hdd_sap_restart_chan_switch_cb(psoc, vdev_id, *ch_freq,
-				       ch_params.ch_width, false);
+	status = hdd_sap_restart_chan_switch_cb(psoc, vdev_id, *ch_freq,
+						ch_params.ch_width, false);
 	wlansap_context_put(sap_context);
 
-	return QDF_STATUS_SUCCESS;
+	return status;
 }
 
 QDF_STATUS

core/hdd/src/wlan_hdd_hostapd.h
@@ -87,13 +87,14 @@ void hdd_stop_sap_set_tx_power(struct wlan_objmgr_psoc *psoc,
  *
  * Invokes the necessary API to perform channel switch for the SAP or GO
  *
- * Return: None
+ * Return: QDF_STATUS_SUCCESS if successfully
  */
-void hdd_sap_restart_with_channel_switch(struct wlan_objmgr_psoc *psoc,
-					 struct hdd_adapter *adapter,
-					 uint32_t target_chan_freq,
-					 uint32_t target_bw,
-					 bool forced);
+QDF_STATUS hdd_sap_restart_with_channel_switch(struct wlan_objmgr_psoc *psoc,
+					       struct hdd_adapter *ap_adapter,
+					       uint32_t target_chan_freq,
+					       uint32_t target_bw,
+					       bool forced);
+
 /**
  * hdd_sap_restart_chan_switch_cb() - Function to restart SAP with
  * a different channel
@@ -104,13 +105,12 @@ void hdd_sap_restart_with_channel_switch(struct wlan_objmgr_psoc *psoc,
  *
  * This function restarts SAP with a different channel
  *
- * Return: None
+ * Return: QDF_STATUS_SUCCESS if successfully
  *
  */
-void hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
-				    uint8_t vdev_id, uint32_t ch_freq,
-				    uint32_t channel_bw,
-				    bool forced);
+QDF_STATUS hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
+					  uint8_t vdev_id, uint32_t ch_freq,
+					  uint32_t channel_bw, bool forced);
 /**
  * wlan_hdd_get_channel_for_sap_restart() - Function to get
  * suitable channel and restart SAP

arter97/caf_qcacld-3.0

core/hdd/src/wlan_hdd_hostapd.c
@@ -3205,10 +3205,10 @@ int hdd_softap_set_channel_change(struct net_device *dev, int target_chan_freq,
 }
 
 #ifdef FEATURE_WLAN_MCC_TO_SCC_SWITCH
-void hdd_sap_restart_with_channel_switch(struct hdd_adapter *ap_adapter,
-					uint32_t target_chan_freq,
-					uint32_t target_bw,
-					bool forced)
+QDF_STATUS hdd_sap_restart_with_channel_switch(struct hdd_adapter *ap_adapter,
+					       uint32_t target_chan_freq,
+					       uint32_t target_bw,
+					       bool forced)
 {
 	struct net_device *dev = ap_adapter->dev;
 	int ret;
@@ -3217,32 +3217,33 @@ void hdd_sap_restart_with_channel_switch(struct hdd_adapter *ap_adapter,
 
 	if (!dev) {
 		hdd_err("Invalid dev pointer");
-		return;
+		return QDF_STATUS_E_INVAL;
 	}
 
 	ret = hdd_softap_set_channel_change(dev, target_chan_freq,
 					    target_bw, forced);
 	if (ret) {
 		hdd_err("channel switch failed");
-		return;
 	}
+
+	return qdf_status_from_os_return(ret);
 }
 
-void hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
-				    uint8_t vdev_id, uint32_t ch_freq,
-				    uint32_t channel_bw,
-				    bool forced)
+QDF_STATUS hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
+					  uint8_t vdev_id, uint32_t ch_freq,
+					  uint32_t channel_bw, bool forced)
 {
 	struct hdd_adapter *ap_adapter =
 		wlan_hdd_get_adapter_from_vdev(psoc, vdev_id);
 
 	if (!ap_adapter) {
 		hdd_err("Adapter is NULL");
-		return;
+		return QDF_STATUS_E_INVAL;
 	}
-	hdd_sap_restart_with_channel_switch(ap_adapter,
-					    ch_freq,
-					    channel_bw, forced);
+
+	return hdd_sap_restart_with_channel_switch(ap_adapter,
+						   ch_freq,
+						   channel_bw, forced);
 }
 
 void wlan_hdd_set_sap_csa_reason(struct wlan_objmgr_psoc *psoc, uint8_t vdev_id,
@@ -3275,6 +3276,7 @@ QDF_STATUS wlan_hdd_get_channel_for_sap_restart(
 	struct sap_context *sap_context;
 	enum sap_csa_reason_code csa_reason =
 		CSA_REASON_CONCURRENT_STA_CHANGED_CHANNEL;
+	QDF_STATUS status;
 
 	if (!ap_adapter) {
 		hdd_err("ap_adapter is NULL");
@@ -3394,11 +3396,11 @@ sap_restart:
 	hdd_ap_ctx->bss_stop_reason = BSS_STOP_DUE_TO_MCC_SCC_SWITCH;
 	*ch_freq = intf_ch_freq;
 	hdd_debug("SAP channel change with CSA/ECSA");
-	hdd_sap_restart_chan_switch_cb(psoc, vdev_id, *ch_freq,
-				       ch_params.ch_width, false);
+	status = hdd_sap_restart_chan_switch_cb(psoc, vdev_id, *ch_freq,
+						ch_params.ch_width, false);
 	wlansap_context_put(sap_context);
 
-	return QDF_STATUS_SUCCESS;
+	return status;
 }
 
 QDF_STATUS

core/hdd/src/wlan_hdd_hostapd.h
@@ -71,12 +71,13 @@ int hdd_softap_set_channel_change(struct net_device *dev,
  *
  * Invokes the necessary API to perform channel switch for the SAP or GO
  *
- * Return: None
+ * Return: QDF_STATUS_SUCCESS if successfully
  */
-void hdd_sap_restart_with_channel_switch(struct hdd_adapter *adapter,
-				uint32_t target_chan_freq,
-				uint32_t target_bw,
-				bool forced);
+QDF_STATUS hdd_sap_restart_with_channel_switch(struct hdd_adapter *ap_adapter,
+					       uint32_t target_chan_freq,
+					       uint32_t target_bw,
+					       bool forced);
+
 /**
  * hdd_sap_restart_chan_switch_cb() - Function to restart SAP with
  * a different channel
@@ -87,13 +88,12 @@ void hdd_sap_restart_with_channel_switch(struct hdd_adapter *adapter,
  *
  * This function restarts SAP with a different channel
  *
- * Return: None
+ * Return: QDF_STATUS_SUCCESS if successfully
  *
  */
-void hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
-				    uint8_t vdev_id, uint32_t ch_freq,
-				    uint32_t channel_bw,
-				    bool forced);
+QDF_STATUS hdd_sap_restart_chan_switch_cb(struct wlan_objmgr_psoc *psoc,
+					  uint8_t vdev_id, uint32_t ch_freq,
+					  uint32_t channel_bw, bool forced);
 /**
  * wlan_hdd_get_channel_for_sap_restart() - Function to get
  * suitable channel and restart SAP