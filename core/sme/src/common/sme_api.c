d0255f73db1cd90285bef64bcccb188c77e63c1b
Aditya Kodukula <quic_akodukul@quicinc.com>
2022-07-09 16:15:03 -0700

    qcacld-3.0: Avoid double free in sch_gen_timing_advert_frame
    In sch_gen_timing_advert_frame, the memory allocated for buffer
    timing_advert->template_value is freed but not reset to NULL.
    This creates a dangling pointer, and it is freed again inside
    __wlan_hdd_cfg80211_ocb_start_timing_advert.
    
    To avoid this issue, reset the pointer to buffer
    timing_advert->template_value to NULL before returning from
    sch_gen_timing_advert_frame.
    
    Change-Id: I2445c53f217d0fd22cbe3026b0869284fe13b851
    CRs-Fixed: 3229906

--- core/sme/src/common/sme_api.c
+++ core/sme/src/common/sme_api.c
@@ -7941,7 +7941,7 @@ void sme_get_command_q_status(mac_handle_t mac_handle)
  * @timestamp_offset: return for the offset of the timestamp field
  * @time_value_offset: return for the time_value field in the TA IE
  *
- * Return: the length of the buffer.
+ * Return: the length of the buffer on success and error on failure.
  */
 int sme_ocb_gen_timing_advert_frame(mac_handle_t mac_handle,
 				    tSirMacAddr self_addr, uint8_t **buf,
