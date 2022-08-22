92163c7875327cdb8cb578d6b0c9c30a564763d6
Sajini R <quic_sajinir@quicinc.com>
2022-08-22 18:20:48 +0530

    qcacmn: Initial changes in UMAC for IPQ5332 target compilation
    Added device ID and target type checks in UMAC for IPQ5332 target
    compilation.
    
    CRs-Fixed: 3273861
    Change-Id: I25f9f8bd6c23b361e3b425744839dce945036bd4

--- umac/cfr/dispatcher/src/wlan_cfr_tgt_api.c
+++ umac/cfr/dispatcher/src/wlan_cfr_tgt_api.c
@@ -153,6 +153,7 @@ int tgt_cfr_validate_period(struct wlan_objmgr_psoc *psoc, u_int32_t period)
 	    target_type == TARGET_TYPE_QCA6018 ||
 	    target_type == TARGET_TYPE_QCA8074V2 ||
 	    target_type == TARGET_TYPE_QCA5018 ||
+	    target_type == TARGET_TYPE_QCA5332 ||
 	    target_type == TARGET_TYPE_QCN9224) {
 		/* No additional check required for these targets */
 		status = 1;
