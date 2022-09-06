52a59a28713972fbb39e24aa092065c5b3883696
Miaoqing Pan <quic_miaoqing@quicinc.com>
2022-09-06 15:01:53 +0800

    qcacld-3.0: flush batched GRO_NORMAL packets
    Kernel 5.4 has applied batched GRO_NORMAL packets processing
    for all napi_gro_receive() users. this requires NAPI users to
    call napi_complete_done() or napi_complete() at the end of every
    polling to flush batched GRO_NORMAL packets.
    However, by current wlan driver design, napi_gro_recevie()
    is not happened in same NAPI polling context and done in another
    thread context, it likely packets which have not been flushed from
    napi->rx_list will get stall until next RX cycle.
    
    Fix this by adding a manual flushing of the list right after
    napi_gro_flush() call to mimic napi_complete() logics.
    
    CRs-Fixed: 3276014
    Change-Id: Ic411853dc9c4330922baf46ceab94d9b19847d58

--- core/hdd/src/wlan_hdd_tx_rx.c
+++ core/hdd/src/wlan_hdd_tx_rx.c
@@ -1564,6 +1564,20 @@ static bool hdd_is_arp_local(struct sk_buff *skb)
 }
 
 /**
+ * When bus bandwidth is idle, if RX data is delivered with
+ * napi_gro_receive, to reduce RX delay related with GRO,
+ * check gro_result returned from napi_gro_receive to determine
+ * is extra GRO flush still necessary.
+ */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
+#define HDD_IS_EXTRA_GRO_FLUSH_NECESSARY(_gro_ret) \
+	((_gro_ret) != GRO_DROP)
+#else
+#define HDD_IS_EXTRA_GRO_FLUSH_NECESSARY(_gro_ret) \
+	((_gro_ret) != GRO_DROP && (_gro_ret) != GRO_NORMAL)
+#endif
+
+/**
  * hdd_is_rx_wake_lock_needed() - check if wake lock is needed
  * @skb: pointer to sk_buff
  *
@@ -1631,7 +1645,7 @@ static QDF_STATUS hdd_gro_rx_bh_disable(struct hdd_adapter *adapter,
 {
 	QDF_STATUS status = QDF_STATUS_SUCCESS;
 	struct hdd_context *hdd_ctx = adapter->hdd_ctx;
-	gro_result_t gro_res;
+	gro_result_t gro_ret;
 	uint32_t rx_aggregation;
 	uint8_t rx_ctx_id = QDF_NBUF_CB_RX_CTX_ID(skb);
 
@@ -1640,14 +1654,14 @@ static QDF_STATUS hdd_gro_rx_bh_disable(struct hdd_adapter *adapter,
 	skb_set_hash(skb, QDF_NBUF_CB_RX_FLOW_ID(skb), PKT_HASH_TYPE_L4);
 
 	local_bh_disable();
-	gro_res = napi_gro_receive(napi_to_use, skb);
+	gro_ret = napi_gro_receive(napi_to_use, skb);
 
 	if (hdd_get_current_throughput_level(hdd_ctx) == PLD_BUS_WIDTH_IDLE ||
 	    !rx_aggregation || adapter->gro_disallowed[rx_ctx_id]) {
-		if (gro_res != GRO_DROP && gro_res != GRO_NORMAL) {
+		if (HDD_IS_EXTRA_GRO_FLUSH_NECESSARY(gro_ret)) {
 			adapter->hdd_stats.tx_rx_stats.
 					rx_gro_low_tput_flush++;
-			napi_gro_flush(napi_to_use, false);
+			dp_rx_napi_gro_flush(napi_to_use);
 		}
 		if (!rx_aggregation)
 			hdd_ctx->dp_agg_param.gro_force_flush[rx_ctx_id] = 1;
@@ -1656,7 +1670,7 @@ static QDF_STATUS hdd_gro_rx_bh_disable(struct hdd_adapter *adapter,
 	}
 	local_bh_enable();
 
-	if (gro_res == GRO_DROP)
+	if (gro_ret == GRO_DROP)
 		status = QDF_STATUS_E_GRO_DROP;
 
 	return status;
@@ -1802,7 +1816,7 @@ static void hdd_rxthread_napi_gro_flush(void *data)
 	 * As we are breaking context in Rxthread mode, there is rx_thread NAPI
 	 * corresponds each hif_napi.
 	 */
-	napi_gro_flush(&qca_napii->rx_thread_napi, false);
+	dp_rx_napi_gro_flush(&qca_napii->rx_thread_napi);
 	local_bh_enable();
 }
 
