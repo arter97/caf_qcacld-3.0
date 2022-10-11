9b699d0eb9166a128e686823ff9dcbc41b5d6393
Hariharan Basuthkar <quic_hbasuthk@quicinc.com>
2022-10-11 05:32:56 +0530

    qcacmn: Add WMI_SERVICE_EIRP_PREFERRED_SUPPORT service bit
    Add WMI support for WMI_SERVICE_EIRP_PREFERRED_SUPPORT service bit,
    which indicates that the target prefers EIRP in the
    WMI_VDEV_SET_TPC_POWER_CMD_ID.
    
    Change-Id: I1433bd698e124336082e12f509f271c9dd7df40a
    CRs-Fixed: 3307372

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -5807,6 +5807,7 @@ typedef enum {
 	wmi_service_ndi_txbf_support,
 	wmi_service_reg_cc_ext_event_support,
 	wmi_service_bang_radar_320_support,
+	wmi_service_eirp_preferred_support,
 #if defined(CONFIG_BAND_6GHZ)
 	wmi_service_lower_6g_edge_ch_supp,
 	wmi_service_disable_upper_6g_edge_ch_supp,
