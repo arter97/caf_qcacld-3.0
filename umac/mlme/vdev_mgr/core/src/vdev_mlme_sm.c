f0811fb133bad3cfeea436a4364cac819245f6ff
Santosh Anbu <quic_sanbu@quicinc.com>
2022-11-04 23:34:52 +0530

    qcacmn: Handle CSA RESTART in MLO SYNC WAIT state
    In case of MLD, if one of the partner vdev is not brought up and
    CSA is triggered on self pdev with multiple vdevs configured then
    all the vdevs of this pdev will move to restart state.
    But the ML AP vdev in MLO_SYNC_WAIT will not handle CSA restart
    leading to multivdev restart request timeout.
    
    Add change to handle CSA_RESTART when the ML AP vdev is in MLO_SYNC_WAIT
    state. Thereby all vdevs of this pdev will move to restart state.
    
    Change-Id: I9b729e2f81d3c72ab5e321f3a396f157424c1663
    CRs-Fixed: 3328931

--- umac/mlme/vdev_mgr/core/src/vdev_mlme_sm.c
+++ umac/mlme/vdev_mgr/core/src/vdev_mlme_sm.c
@@ -1711,6 +1711,7 @@ static bool mlme_vdev_subst_mlo_sync_wait_event(void *ctx, uint16_t event,
 		break;
 
 	case WLAN_VDEV_SM_EV_RADAR_DETECTED:
+	case WLAN_VDEV_SM_EV_CSA_RESTART:
 		mlme_vdev_sm_transition_to(vdev_mlme, WLAN_VDEV_S_START);
 		mlme_vdev_sm_deliver_event(vdev_mlme,
 					   WLAN_VDEV_SM_EV_RESTART_REQ,
