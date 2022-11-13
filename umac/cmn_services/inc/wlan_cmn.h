2a525d754524a822c7396f4ce4fb5babe0d34acb
Yeshwanth Sriram Guntuka <quic_ysriramg@quicinc.com>
2022-11-13 22:40:05 +0530

    qcacmn: Add id for QMI component
    Add QMI component entry in object manager debug id
    and umac component id.
    
    Change-Id: Ib2531ee9cc91f721214f9e30b6b25cb7f3e89a42
    CRs-Fixed: 3338296

--- umac/cmn_services/inc/wlan_cmn.h
+++ umac/cmn_services/inc/wlan_cmn.h
@@ -296,6 +296,7 @@
  * @WLAN_UMAC_COMP_PRE_CAC:       PRE CAC component
  * @WLAN_COMP_DP:                 DP component
  * @WLAN_UMAC_COMP_COAP:          Constrained Application Protocol component
+ * @WLAN_UMAC_COMP_QMI:           QMI component
  * @WLAN_UMAC_COMP_ID_MAX:        Maximum components in UMAC
  *
  * This id is static.
@@ -351,6 +352,7 @@ enum wlan_umac_comp_id {
 	WLAN_COMP_DP                      = 46,
 	WLAN_COMP_TELEMETRY_AGENT         = 47,
 	WLAN_UMAC_COMP_COAP               = 48,
+	WLAN_UMAC_COMP_QMI                = 49,
 	WLAN_UMAC_COMP_ID_MAX,
 };
 
