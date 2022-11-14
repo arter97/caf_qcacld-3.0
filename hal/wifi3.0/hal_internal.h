a7858e3991b60b084e5e28ec42ac41c50754ab1f
Pavankumar Nandeshwar <quic_pnandesh@quicinc.com>
2022-11-13 23:45:13 -0800

    qcacmn: Retain ds ring indices across wifi restart
    Retain ds ring indices across wifi restart to avoid
    edma hang. Fetch the indices from ds module and set
    the corresponding ds ring indices.
    
    Change-Id: Ia299a7006166aef096c7d2c1f65f6bef65415a37
    CRs-Fixed: 3332152

--- hal/wifi3.0/hal_internal.h
+++ hal/wifi3.0/hal_internal.h
@@ -823,9 +823,11 @@ struct hal_rx_pkt_capture_flags {
 struct hal_hw_txrx_ops {
 	/* init and setup */
 	void (*hal_srng_dst_hw_init)(struct hal_soc *hal,
-				     struct hal_srng *srng, bool idle_check);
+				     struct hal_srng *srng, bool idle_check,
+				     uint32_t idx);
 	void (*hal_srng_src_hw_init)(struct hal_soc *hal,
-				     struct hal_srng *srng, bool idle_check);
+				     struct hal_srng *srng, bool idle_check,
+				     uint32_t idx);
 
 	void (*hal_srng_hw_disable)(struct hal_soc *hal,
 				    struct hal_srng *srng);
@@ -1217,6 +1219,9 @@ struct hal_hw_txrx_ops {
 	QDF_STATUS (*hal_srng_set_msi_config)(hal_ring_handle_t ring_hdl,
 					      void *ring_params);
 #endif
+	void (*hal_tx_ring_halt_set)(hal_soc_handle_t hal_soc_hdl);
+	void (*hal_tx_ring_halt_reset)(hal_soc_handle_t hal_soc_hdl);
+	bool (*hal_tx_ring_halt_poll)(hal_soc_handle_t hal_soc_hdl);
 };
 
 /**
