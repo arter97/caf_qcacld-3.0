e97969c98d39f7c15e461f52f67637032e12e027
Harsh Kumar Bijlani <quic_hbijlani@quicinc.com>
2022-12-13 17:55:22 +0530

    qca-wifi: Add vap level commands to set and get drop_tx_mcast
    Add vap level commands to set and get drop_tx_mcast using
    cfg80211tool.
    
    Change-Id: I4ad50b43ce552a0d9113d91a4629d7dfc70880b5
    CRs-Fixed: 3359396

--- tools/linux/cfg80211_ven_cmd.h
+++ tools/linux/cfg80211_ven_cmd.h
@@ -834,6 +834,9 @@ enum {
 #ifdef WLAN_FEATURE_11BE
 	IEEE80211_PARAM_EHT_CONFIG_CCFS0 = 789, /* Set 11be - EHT Config CCFS0 in 6GHz */
 #endif /* WLAN_FEATURE_11BE */
+#ifdef QCA_SUPPORT_WDS_EXTENDED
+	IEEE80211_PARAM_DROP_TX_MCAST  = 790,  /* Flag to enable/disable tx mcast pkt drops */
+#endif
 };
 
 enum {
@@ -2508,6 +2511,10 @@ struct vendor_commands vap_vendor_cmds[] = {
 	{"eht_config_ccfs0",	IEEE80211_PARAM_EHT_CONFIG_CCFS0, SET_PARAM, 1},
 	{"get_eht_config_ccfs0",	IEEE80211_PARAM_EHT_CONFIG_CCFS0, GET_PARAM, 0},
 #endif /* WLAN_FEATURE_11BE */
+#ifdef QCA_SUPPORT_WDS_EXTENDED
+	{"drop_tx_mcast",      IEEE80211_PARAM_DROP_TX_MCAST, SET_PARAM, 1},
+	{"get_drop_tx_mcast",  IEEE80211_PARAM_DROP_TX_MCAST, GET_PARAM, 0},
+#endif
 };
 
 struct vendor_commands radio_vendor_cmds[] = {
