LineageOS/android_kernel_xiaomi_sdm660

qcacld-3.0: Allow PMKID only entries also to pmksa cache
In SAP case, external authentication(offloaded SAE authentication
to userspace) status event carries only PMKID to allow/disallow
peers to do PMKID+open auth based connection once the peers are
already SAE authenticated.
A check is added on the PMK length with the below change,
change: If1116d6a93747b88494ee81239759d5f212b9eca. It rejects the
PMKID update to the pmk cache if the PMK length is 0. It's not
allowing to cache PMKID derived as part of SAE authentication.
There is no need to cache the PMK in SAE case as it won't be
used internally by driver or won't even be sent to firmware.
So, don't add any check and allow PMKID only entries also to
facilitate PMK caching mechanism in SAE SAP.

Also skip 2 STA only code changes:
If81d66059173dda8d7a2940ca10dc96ef16e125c
Id960e955f8669e19e950c1edc38a9f0a715292b7

Change-Id: I8eb8747b7bee8a74b9cd3d7511f8777d2359267b
CRs-Fixed: 3091396

core/sme/src/csr/csr_api_roam.c
@@ -15345,6 +15345,7 @@ csr_roam_set_pmkid_cache(struct mac_context *mac, uint32_t sessionId,
 	tPmkidCacheInfo *pmksa, *pmkid_cache;
 	enum csr_akm_type akm_type;
 	QDF_STATUS status = QDF_STATUS_E_FAILURE;
+	enum QDF_OPMODE op_mode;
 
 	if (!pSession) {
 		sme_err("session %d not found", sessionId);
@@ -15376,23 +15377,30 @@ csr_roam_set_pmkid_cache(struct mac_context *mac, uint32_t sessionId,
 
 	for (i = 0; i < numItems; i++) {
 		pmksa = &pPMKIDCache[i];
+		op_mode = csr_get_session_persona(mac, sessionId);
+		sme_debug("op_mode %d", op_mode);
 
-		if (!pmksa->pmk_len || pmksa->pmk_len > CSR_RSN_MAX_PMK_LEN) {
-			sme_err("Invalid PMK length");
-			status = QDF_STATUS_E_FAILURE;
-			goto error;
-		}
-		qdf_copy_macaddr(&pmkid_cache->BSSID, &pmksa->BSSID);
-		sme_debug("Trying to find PMKID for " QDF_MAC_ADDR_STR,
-			  QDF_MAC_ADDR_ARRAY(pmkid_cache->BSSID.bytes));
-		if (csr_lookup_pmkid_using_bssid(mac, pSession, pmkid_cache,
-						 &pmkid_index) &&
-		    (!qdf_mem_cmp(pmkid_cache->pmk,
-				  pmksa->pmk, pmksa->pmk_len))) {
-			sme_debug("PMKSA entry found with same PMK at index %d",
-				  pmkid_index);
-			status = QDF_STATUS_E_EXISTS;
-			goto error;
+		/* Below code only for STA mode case, skip them for SAP mode*/
+		if (op_mode != QDF_SAP_MODE) {
+			if (!pmksa->pmk_len ||
+			    pmksa->pmk_len > CSR_RSN_MAX_PMK_LEN) {
+				sme_err("Invalid PMK length");
+				status = QDF_STATUS_E_FAILURE;
+				goto error;
+			}
+			qdf_copy_macaddr(&pmkid_cache->BSSID, &pmksa->BSSID);
+			sme_debug("Trying to find PMKID for " QDF_MAC_ADDR_STR,
+				  QDF_MAC_ADDR_ARRAY(pmkid_cache->BSSID.bytes));
+			if (csr_lookup_pmkid_using_bssid(mac, pSession,
+							 pmkid_cache,
+							 &pmkid_index) &&
+			    (!qdf_mem_cmp(pmkid_cache->pmk,
+					  pmksa->pmk, pmksa->pmk_len))) {
+				sme_debug("PMKSA entry found with same PMK at index %d",
+					  pmkid_index);
+				status = QDF_STATUS_E_EXISTS;
+				goto error;
+			}
 		}
 
 		/* Delete the entry if present */
@@ -15400,6 +15408,11 @@ csr_roam_set_pmkid_cache(struct mac_context *mac, uint32_t sessionId,
 		/* Update new entry */
 		csr_update_pmk_cache(pSession, pmksa);
 
+		/* Below code only for STA mode case, skip them for SAP mode*/
+		if (op_mode == QDF_SAP_MODE) {
+			status = QDF_STATUS_SUCCESS;
+			continue;
+		}
 		akm_type = pSession->connectedProfile.AuthType;
 		if ((akm_type == eCSR_AUTH_TYPE_FT_RSN ||
 		     akm_type == eCSR_AUTH_TYPE_FT_FILS_SHA256 ||

arter97/caf_qcacld-3.0

core/sme/src/csr/csr_api_roam.c
@@ -15985,6 +15985,7 @@ csr_roam_set_pmkid_cache(tpAniSirGlobal pMac, uint32_t sessionId,
 	uint32_t pmkid_index;
 	tPmkidCacheInfo *pmksa, *pmkid_cache;
 	eCsrAuthType akm_type;
+	enum QDF_OPMODE op_mode;
 
 	if (!pSession) {
 		sme_err("session %d not found", sessionId);
@@ -16016,25 +16017,32 @@ csr_roam_set_pmkid_cache(tpAniSirGlobal pMac, uint32_t sessionId,
 
 	for (i = 0; i < numItems; i++) {
 		pmksa = &pPMKIDCache[i];
+		op_mode = csr_get_session_persona(pMac, sessionId);
+		sme_debug("op_mode %d", op_mode);
 
-		if (!pmksa->pmk_len || pmksa->pmk_len > CSR_RSN_MAX_PMK_LEN) {
-			sme_err("Invalid PMK length");
-			qdf_mem_zero(pmkid_cache, sizeof(*pmkid_cache));
-			qdf_mem_free(pmkid_cache);
-			return QDF_STATUS_E_FAILURE;
-		}
-		qdf_copy_macaddr(&pmkid_cache->BSSID, &pmksa->BSSID);
-		sme_debug("Trying to find PMKID for " QDF_MAC_ADDR_STR,
-			  QDF_MAC_ADDR_ARRAY(pmkid_cache->BSSID.bytes));
-		if (csr_lookup_pmkid_using_bssid(pMac, pSession, pmkid_cache,
-						 &pmkid_index) &&
-		    (!qdf_mem_cmp(pmkid_cache->pmk,
-				  pmksa->pmk, pmksa->pmk_len))) {
-			sme_debug("PMKSA entry found with same PMK at index %d",
-				  pmkid_index);
-			qdf_mem_zero(pmkid_cache, sizeof(*pmkid_cache));
-			qdf_mem_free(pmkid_cache);
-			return QDF_STATUS_E_EXISTS;
+		/* Below code only for STA mode case, skip them for SAP mode*/
+		if (op_mode != QDF_SAP_MODE) {
+			if (!pmksa->pmk_len ||
+			    pmksa->pmk_len > CSR_RSN_MAX_PMK_LEN) {
+				sme_err("Invalid PMK length");
+				qdf_mem_zero(pmkid_cache, sizeof(*pmkid_cache));
+				qdf_mem_free(pmkid_cache);
+				return QDF_STATUS_E_FAILURE;
+			}
+			qdf_copy_macaddr(&pmkid_cache->BSSID, &pmksa->BSSID);
+			sme_debug("Trying to find PMKID for " QDF_MAC_ADDR_STR,
+				  QDF_MAC_ADDR_ARRAY(pmkid_cache->BSSID.bytes));
+			if (csr_lookup_pmkid_using_bssid(pMac, pSession,
+							 pmkid_cache,
+							 &pmkid_index) &&
+			    (!qdf_mem_cmp(pmkid_cache->pmk,
+					  pmksa->pmk, pmksa->pmk_len))) {
+				sme_debug("PMKSA entry found with same PMK at index %d",
+					  pmkid_index);
+				qdf_mem_zero(pmkid_cache, sizeof(*pmkid_cache));
+				qdf_mem_free(pmkid_cache);
+				return QDF_STATUS_E_EXISTS;
+			}
 		}
 
 		/* Delete the entry if present */
@@ -16042,6 +16050,10 @@ csr_roam_set_pmkid_cache(tpAniSirGlobal pMac, uint32_t sessionId,
 		/* Update new entry */
 		csr_update_pmk_cache(pSession, pmksa);
 
+		/* Below code only for STA mode case, skip them for SAP mode*/
+		if (op_mode == QDF_SAP_MODE) {
+			continue;
+		}
 		akm_type = pSession->connectedProfile.AuthType;
 		if ((akm_type == eCSR_AUTH_TYPE_FT_RSN ||
 		     akm_type == eCSR_AUTH_TYPE_FT_FILS_SHA256 ||