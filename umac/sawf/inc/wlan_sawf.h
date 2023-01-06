d982226bc59523007dbaa514eccca2a54f23910b
Nandha Kishore Easwaran <quic_nandhaki@quicinc.com>
2023-01-06 11:37:38 +0530

    qca-wifi: Disable the service class from peer delete
    Disable the service class added by SCS from peer delete path if both
    ref count and peer count are zero.
    
    Change-Id: Id782e91a049c1f9a0749a09c6cfb31eff7cb4cc2

--- umac/sawf/inc/wlan_sawf.h
+++ umac/sawf/inc/wlan_sawf.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -456,4 +456,12 @@ void wlan_service_id_dec_peer_count(uint8_t svc_id);
  * Return: void
  */
 void wlan_service_id_inc_peer_count(uint8_t svc_id);
+
+/* wlan_disable_service_class() - Disable service class
+ *
+ * @svc_id : service-class id
+ *
+ * Return: void
+ */
+void wlan_disable_service_class(uint8_t svc_id);
 #endif
