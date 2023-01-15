3a1345790613124db742a2cec4bd78e601839e3d
Jeff Johnson <quic_jjohnson@quicinc.com>
2023-01-15 12:36:14 -0800

    qcacmn: Fix umac/scan documentation
    The kernel-doc script identified some documentation errors in the
    umac/scan folder, so fix them.
    
    In addition, Change-Id I8340c6c654cf03f63aee9ed622e3db38056d714f
    ("qcacmn: Add API to access scan entry partner info") made an
    incorrect copyright change, so fix that.
    
    Change-Id: I629f5961ab0a284c70a47fc7d71fe81d5572ae25
    CRs-Fixed: 3380633

--- umac/scan/dispatcher/inc/cfg_scan.h
+++ umac/scan/dispatcher/inc/cfg_scan.h
@@ -44,6 +44,7 @@
  *  channels and doesn't set the flag FLAG_SCAN_ONLY_IF_RNR_FOUND for any 6g
  *  (PSC/non-PSC) channels. Rest of the scans will be done only on RNR (PSC and
  *  non-PSC channels).
+ * @SCAN_MODE_6G_MAX: Max enumeration
  */
 enum scan_mode_6ghz {
 	SCAN_MODE_6G_NO_CHANNEL,
