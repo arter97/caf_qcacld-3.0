ff002128c9643a96b0ee39ac85d1bd722c85c8c4
Wade Song <quic_wadesong@quicinc.com>
2023-01-20 15:51:11 +0800

    qcacmn: Increase the number of pcie slots supported
    With some new platforms planned, 5 pcie slots have to be
    supported by the wlan driver.
    
    Increase WLAN_CFG_MAX_PCIE_GROUPS to 5 to make sure wlan
    probe is successful even when the wlan card is attached
    to the last pcie slot.
    
    Change-Id: Iab040a26e58b9d95321c29f88f87b7923198b7dd
    CRs-Fixed: 3390735

--- hif/src/pcie/if_pci.c
+++ hif/src/pcie/if_pci.c
@@ -177,7 +177,7 @@ char *legacy_ic_irqname[] = {
  * use TargetCPU warm reset * instead of SOC_GLOBAL_RESET
  */
 #define CPU_WARM_RESET_WAR
-#define WLAN_CFG_MAX_PCIE_GROUPS 4
+#define WLAN_CFG_MAX_PCIE_GROUPS 5
 #ifdef QCA_WIFI_QCN9224
 #define WLAN_CFG_MAX_CE_COUNT 16
 #else
