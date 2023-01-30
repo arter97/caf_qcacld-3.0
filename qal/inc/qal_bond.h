182750de22d91cf3c556588f0ff0f3701b334053
Shivani Soni <quic_soni@quicinc.com>
2023-01-30 15:17:36 +0530

    qca-wifi: Kernel docs for qal bond
    Kernel docs added for qal abstraction for bond module
    
    Change-Id: I6bd6f9f18652b3930a4faee09cdacd88a3c21dad

--- qal/inc/qal_bond.h
+++ qal/inc/qal_bond.h
@@ -26,16 +26,60 @@
 #include <net/cfg80211.h>
 #include <net/bonding.h>
 
+/**
+ *
+ * qal_bond_create() - Create bond netdevice
+ *
+ * @net: net structure
+ * @name: Bond netdevice name
+ * @mlo_info: mlo_bond_info private structure
+ *
+ * Return: Created bond netdevice
+ */
 struct net_device *qal_bond_create(struct net *net,
-				   char *name, struct mlo_bond_info *mlo_info);
+				   char *name, void *mlo_info);
 
+/**
+ *
+ * qal_bond_create_slave() - Add slave netdevice to bond netdevice
+ *
+ * @bond_dev: Bond netdevice i.e. master
+ * @slave_dev: Slave netdevice which will be enslaved to master
+ *
+ * Return: 0 for success, -1 for failure
+ */
 int qal_bond_create_slave(struct net_device *bond_dev,
 			  struct net_device *slave_dev);
 
+/**
+ *
+ * qal_bond_slave_release() - Release slave netdevice from bond netdevice
+ *
+ * @bond_dev: Bond netdevice i.e. master
+ * @slave_dev: Slave netdevice which will be released from master
+ *
+ * Return: 0 for success, -1 for failure
+ */
 int qal_bond_slave_release(struct net_device *bond_dev,
 			   struct net_device *slave_dev);
 
+/**
+ *
+ * qal_bond_destroy() - Destroy bond netdevice
+ *
+ * @bond_dev: Bond netdevice to be destroyed
+ *
+ * Return: 0 for success, -1 for failure
+ */
 int qal_bond_destroy(struct net_device *bond_dev);
 
+/**
+ *
+ * qal_bond_get_mlo_ctx() - Get bond mlo context
+ *
+ * @bond_dev: Bond netdevice
+ *
+ * Return: bond mlo context
+ */
 void *qal_bond_get_mlo_ctx(struct net_device *bond_dev);
 #endif /* __QAL_BOND_H */
