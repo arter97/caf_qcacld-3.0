459cea50e220ccdca9e0202fd2169095274f10e7
Asutosh Mohapatra <quic_asutmoha@quicinc.com>
2023-02-13 01:34:52 -0800

    qcacmn: Find best 6 GHz power type for connection
    Find the best 6 GHz power type for connection
    according to following regulatory policy:
    1) SP power type is selected only if AP advertises
       SP and client supports SP.
    2) LPI power type is selected only if AP advertises
       LPI and client supports LPI.
    3) VLP power type is selected for the below cases,
       a) AP advertises VLP and client supports VLP
       b) AP advertises SP but client doesn't support
          SP but supports VLP.
       c) AP advertises LPI but client doesn't support
          LPI but supports VLP.
    
    Change-Id: I6e3b9fc4bf7445c58681ef0ea8e45b434851a5b6
    CRs-Fixed: 3456182

--- umac/regulatory/dispatcher/src/wlan_reg_services_api.c
+++ umac/regulatory/dispatcher/src/wlan_reg_services_api.c
@@ -104,16 +104,13 @@ qdf_export_symbol(wlan_reg_get_pwrmode_chan_list);
 
 #ifdef CONFIG_REG_CLIENT
 QDF_STATUS
-wlan_reg_get_6g_power_type_for_ctry(struct wlan_objmgr_psoc *psoc,
-				    struct wlan_objmgr_pdev *pdev,
-				    uint8_t *ap_ctry, uint8_t *sta_ctry,
-				    enum reg_6g_ap_type *pwr_type_6g,
-				    bool *ctry_code_match,
-				    enum reg_6g_ap_type ap_pwr_type)
+wlan_reg_get_best_6g_power_type(struct wlan_objmgr_psoc *psoc,
+				struct wlan_objmgr_pdev *pdev,
+				enum reg_6g_ap_type *pwr_type_6g,
+				enum reg_6g_ap_type ap_pwr_type)
 {
-	return reg_get_6g_power_type_for_ctry(psoc, pdev, ap_ctry, sta_ctry,
-					      pwr_type_6g, ctry_code_match,
-					      ap_pwr_type);
+	return reg_get_best_6g_power_type(psoc, pdev, pwr_type_6g,
+					  ap_pwr_type);
 }
 #endif
 
