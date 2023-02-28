5600f0123ed3ab8bf266759b545c51125f565b77
Mohammed Ahmed <quic_moah@quicinc.com>
2023-02-27 22:19:51 -0800

    qcacld-3.0: add hdd_dump_log_buffer output to sysfs
    Current code doesn't allow output of hdd_dump_log_buffer to sysfs.
    Fix this by adding sysfs output functionality.
    
    Change-Id: I077c10e379e542ac99d8ebe01d68fe76a7470e39
    CRs-Fixed: 3426193

--- core/hdd/src/wlan_hdd_sysfs.c
+++ core/hdd/src/wlan_hdd_sysfs.c
@@ -90,6 +90,7 @@
 #include <wlan_hdd_sysfs_eht_rate.h>
 #include <wlan_hdd_sysfs_direct_link_ut_cmd.h>
 #include <wlan_hdd_sysfs_runtime_pm.h>
+#include <wlan_hdd_sysfs_log_buffer.h>
 
 #define MAX_PSOC_ID_SIZE 10
 
@@ -939,12 +940,14 @@ void hdd_create_sysfs_files(struct hdd_context *hdd_ctx)
 		hdd_sysfs_get_valid_freq_for_power_create(driver_kobject);
 		hdd_sysfs_dp_pkt_add_ts_create(driver_kobject);
 		hdd_sysfs_runtime_pm_create(driver_kobject);
+		hdd_sysfs_log_buffer_create(driver_kobject);
 	}
 }
 
 void hdd_destroy_sysfs_files(void)
 {
 	if  (QDF_GLOBAL_MISSION_MODE == hdd_get_conparam()) {
+		hdd_sysfs_log_buffer_destroy(driver_kobject);
 		hdd_sysfs_runtime_pm_destroy(driver_kobject);
 		hdd_sysfs_dp_pkt_add_ts_destroy(driver_kobject);
 		hdd_sysfs_get_valid_freq_for_power_destroy(driver_kobject);
