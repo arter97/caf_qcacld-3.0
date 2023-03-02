2c0ca85f39728040768bc5b698220bac16525545
Himanshu Batra <quic_hbatra@quicinc.com>
2023-03-02 12:46:09 +0530

    qcacmn: Add API to fetch ml_peer_valid field from skb cb
    Add API to fetch ml_peer_valid field from skb cb
    
    Change-Id: Ic2bdc8dd2ae03dad6f0234920c1312f234bc4339
    CRs-Fixed: 3423453

--- qdf/linux/src/i_qdf_nbuf_w.h
+++ qdf/linux/src/i_qdf_nbuf_w.h
@@ -429,6 +429,10 @@ QDF_COMPILE_TIME_ASSERT(qdf_nbuf_cb_size,
 	(((struct qdf_nbuf_cb *) \
 	  ((skb)->cb))->u.rx.hw_info.desc_tlv_members.peer_id)
 
+#define QDF_NBUF_CB_RX_ML_PEER_VALID(skb) \
+	(((struct qdf_nbuf_cb *) \
+	  ((skb)->cb))->u.rx.hw_info.desc_tlv_members.ml_peer_valid)
+
 #define QDF_NBUF_CB_RX_VDEV_ID(skb) \
 	(((struct qdf_nbuf_cb *) \
 	  ((skb)->cb))->u.rx.hw_info.desc_tlv_members.vdev_id)
