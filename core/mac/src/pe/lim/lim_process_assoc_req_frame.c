af0befa702d7eab6a7ef2258e65256044fad2006
Ben Wang <quic_benw@quicinc.com>
2023-03-06 08:42:01 +0530

    qcacld-3.0: Fix one crash during SAP fast BSS transition
    If STA is already present in DPH, don't need to clean it during FT
    re-assoc request received. dph_delete_hash_entry doesn't
    clean pmfSaQueryTimer. During driver unloading, host will trigger a
    crash if any pmfSaQueryTimer haven't clean up.
    
    Change-Id: I94ab862ae87c114240e10e123bb9f38eb354514e
    CRs-Fixed: 3422397

--- core/mac/src/pe/lim/lim_process_assoc_req_frame.c
+++ core/mac/src/pe/lim/lim_process_assoc_req_frame.c
@@ -2603,14 +2603,6 @@ void lim_process_assoc_req_frame(struct mac_context *mac_ctx,
 			sub_type, GET_LIM_SYSTEM_ROLE(session),
 			QDF_MAC_ADDR_REF(hdr->sa));
 			return;
-		} else if (sta_ds->mlmStaContext.akm_type == ANI_AKM_TYPE_FT_RSN_PSK) {
-			pe_debug("FT Assoc Req, delete STA hash entry");
-			lim_release_peer_idx(mac_ctx, sta_ds->assocId, session);
-			if (dph_delete_hash_entry(mac_ctx, hdr->sa,
-						  sta_ds->assocId,
-						  &session->dph.dphHashTable)
-			    != QDF_STATUS_SUCCESS)
-				pe_err("error deleting hash entry");
 		} else if (!sta_ds->rmfEnabled && (sub_type == LIM_REASSOC)) {
 			/*
 			 * SAP should send reassoc response with reject code
