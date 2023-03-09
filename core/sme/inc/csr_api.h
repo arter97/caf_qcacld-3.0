8242cb38ff31b86fbca1496e11bd7bc0b00a3cb6
Jeff Johnson <quic_jjohnson@quicinc.com>
2023-03-08 21:08:38 -0800

    qcacld-3.0: Remove typedef csr_session_open_cb
    The last use of typedef csr_session_open_cb was removed by:
      qcacld-3.0: Register Open/close session callbacks once
      Change-Id Ia94857ce4cc608be30e9974ddde8a6261d45f6e3
    
    Since this typedef is unused, remove it.
    
    Change-Id: I73d21bd04a3a28130e09a40dc71112d1c8af4d62
    CRs-Fixed: 3428822

--- core/sme/inc/csr_api.h
+++ core/sme/inc/csr_api.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2011-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -779,8 +779,6 @@ typedef QDF_STATUS (*csr_roam_complete_cb)(struct wlan_objmgr_psoc *psoc,
 					   struct csr_roam_info *param,
 					   eRoamCmdStatus roam_status,
 					   eCsrRoamResult roam_result);
-typedef QDF_STATUS (*csr_session_open_cb)(uint8_t session_id,
-					  QDF_STATUS qdf_status);
 typedef QDF_STATUS (*csr_session_close_cb)(uint8_t session_id);
 
 #define CSR_IS_ANY_BSS_TYPE(pProfile) (eCSR_BSS_TYPE_ANY == \
