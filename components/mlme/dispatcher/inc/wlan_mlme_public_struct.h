b150d1328c1a490356fcb3a9c8d7c9f712382059
Aditya Kodukula <quic_akodukul@quicinc.com>
2023-03-14 14:53:41 -0700

    qcacld-3.0: Add support for gStaKeepAliveMethod ini
    Currently the ini gStaKeepAliveMethod doesn't support
    Unsolicited ARP response packets. So, add it.
    
    Change-Id: I614e87e7f173ea5c25c34eaf56de0805d37b26a4
    CRs-Fixed: 3433020

--- components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
+++ components/mlme/dispatcher/inc/wlan_mlme_public_struct.h
@@ -1669,12 +1669,14 @@ struct wlan_mlme_nss_chains {
  * @MLME_STA_KEEPALIVE_MIN: ensure KEEPALIVE_NULL or ARP are not values of 0
  * @MLME_STA_KEEPALIVE_NULL_DATA: null data packet
  * @MLME_STA_KEEPALIVE_GRAT_ARP: gratuitous ARP packet
+ * @MLME_STA_KEEPALIVE_UNSOLICIT_ARP_RSP: unsolicited  ARP response packet
  * @MLME_STA_KEEPALIVE_COUNT: number of method options available
  */
 enum station_keepalive_method {
 	MLME_STA_KEEPALIVE_MIN,
 	MLME_STA_KEEPALIVE_NULL_DATA = 1,
 	MLME_STA_KEEPALIVE_GRAT_ARP = 2,
+	MLME_STA_KEEPALIVE_UNSOLICIT_ARP_RSP = 3,
 	/* keep at the end */
 	MLME_STA_KEEPALIVE_COUNT
 };
