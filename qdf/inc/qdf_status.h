149071276c33f02e20224cd7d7bd872abf2a89b1
Namita Nair <quic_namibala@quicinc.com>
2023-03-13 17:50:27 -0700

    qcacmn: WAR for opt_wifi_dp feature to disable IPA(2)
    IPA_OFFLOAD path will be enabled by default for MSM,
    inorder to support optional wifi datapath feature.
    This change is a WAR to disable IPA offload during
    compile time, if the feature is not enabled.
    This is done using the IPA_WDI_OPT_DPATH feature flag
    from IPA. If the flag is not defined in the IPA test
    module file, IPA offload path and optional wifi dp
    feature will be disabled.
    
    This is a WAR, and will be fixed once a Kernel config is
    available from IPA to enable or disable the optional
    wifi datapath feature.
    
    Change-Id: Ic1b961656f52fa009ab4c3b3d8d3cac305c2c7c4
    CRs-Fixed: 3431972

--- qdf/inc/qdf_status.h
+++ qdf/inc/qdf_status.h
@@ -26,6 +26,18 @@
 #ifndef __QDF_STATUS_H
 #define __QDF_STATUS_H
 
+#ifdef IPA_OFFLOAD
+	#ifdef CONFIG_ARCH_KALAMA
+		#include "../../../../dataipa/drivers/platform/msm/ipa/ipa_test_module/ipa_test_module.h"
+		#ifndef IPA_WDI_OPT_DPATH
+			#undef IPA_OFFLOAD
+			#undef IPA_OPT_WIFI_DP
+		#endif
+	#endif
+#else
+	#undef IPA_OPT_WIFI_DP
+#endif
+
 /**
  * typedef QDF_STATUS - QDF error codes
  * @QDF_STATUS_SUCCESS: success
