d82b9641ac3917ec12bdd44ba3031520228f500a
Aditya Kodukula <quic_akodukul@quicinc.com>
2023-03-23 21:51:44 -0700

    qcacld-3.0: Add sysfs replacement for set bitrates
    Add a sysfs replacement for the deprecated WEXT commands
    set11NRates, set11ACRates and set_11ax_rate.
    
    Change-Id: I7823660f377d4d4aeb8df2d88b6a871b9ec33ec2
    CRs-Fixed: 3588225

--- core/hdd/src/wlan_hdd_sysfs.c
+++ core/hdd/src/wlan_hdd_sysfs.c
@@ -94,6 +94,7 @@
 #include <wlan_hdd_sysfs_dfsnol.h>
 #include <wlan_hdd_sysfs_wds_mode.h>
 #include <wlan_hdd_sysfs_roam_trigger_bitmap.h>
+#include <wlan_hdd_sysfs_bitrates.h>
 
 #define MAX_PSOC_ID_SIZE 10
 
@@ -824,11 +825,13 @@ hdd_sysfs_create_sta_adapter_root_obj(struct hdd_adapter *adapter)
 	hdd_sysfs_bmiss_create(adapter);
 	hdd_sysfs_dp_tx_delay_stats_create(adapter);
 	hdd_sysfs_direct_link_ut_cmd_create(adapter);
+	hdd_sysfs_sta_bitrates_create(adapter);
 }
 
 static void
 hdd_sysfs_destroy_sta_adapter_root_obj(struct hdd_adapter *adapter)
 {
+	hdd_sysfs_sta_bitrates_destroy(adapter);
 	hdd_sysfs_direct_link_ut_destroy(adapter);
 	hdd_sysfs_dp_tx_delay_stats_destroy(adapter);
 	hdd_sysfs_bmiss_destroy(adapter);
@@ -887,11 +890,13 @@ hdd_sysfs_create_sap_adapter_root_obj(struct hdd_adapter *adapter)
 	hdd_sysfs_dp_traffic_end_indication_create(adapter);
 	hdd_sysfs_direct_link_ut_cmd_create(adapter);
 	hdd_sysfs_dfsnol_create(adapter);
+	hdd_sysfs_sap_bitrates_create(adapter);
 }
 
 static void
 hdd_sysfs_destroy_sap_adapter_root_obj(struct hdd_adapter *adapter)
 {
+	hdd_sysfs_sap_bitrates_destroy(adapter);
 	hdd_sysfs_dfsnol_destroy(adapter);
 	hdd_sysfs_direct_link_ut_destroy(adapter);
 	hdd_sysfs_dp_traffic_end_indication_destroy(adapter);
