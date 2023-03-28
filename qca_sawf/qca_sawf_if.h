3fc47fa7e5baebb0a7b86fb562f70fd16e6da33c
Sreeramya Soratkal <quic_ssramya@quicinc.com>
2023-03-28 19:00:34 +0530

    qca-wifi: Define a new API to fetch MSDU queue for scs based rule
    For the convergence of SCS and SAWF, the definition of the
    API to fetch the MSDU queue needs additional parameters.
    Define a new API that will be used to fetch the MSDU queue value
    for scs based rules.
    
    CRs-Fixed: 3447676
    Change-Id: I2b8a3fc379cf086b15fe15e44b75339d48952ff8

--- qca_sawf/qca_sawf_if.h
+++ qca_sawf/qca_sawf_if.h
@@ -19,7 +19,9 @@
 
 uint16_t qca_sawf_get_msduq(struct net_device *netdev,
 			    uint8_t *peer_mac, uint32_t service_id);
-
+uint16_t qca_sawf_get_msduq_v2(struct net_device *netdev, uint8_t *peer_mac,
+			       uint32_t service_id, uint32_t dscp,
+			       uint32_t rule_id, bool scs_based_rule);
 uint16_t qca_sawf_get_msdu_queue(struct net_device *netdev,
 				 uint8_t *peer_mac, uint32_t service_id,
 				 uint32_t dscp, uint32_t rule_id);
