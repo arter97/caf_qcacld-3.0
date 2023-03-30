1192e50c58432dac8b9aa24d2419fb7eac6c9977
Nidhi Jain <quic_nidhjain@quicinc.com>
2023-03-30 20:11:34 +0530

    qcacmn: Set 11az TBR Responder related feature capabilities
    The 11az Trigger Based Ranging for responder related feature
    capability is not set currently.
    Add 11az TBR responder capabilities.
    
    Change-Id: I0bd9a88231dfc7200895001d109aecdaef4aed59
    CRs-Fixed: 3436972

--- os_if/linux/wifi_pos/src/wlan_cfg80211_wifi_pos.c
+++ os_if/linux/wifi_pos/src/wlan_cfg80211_wifi_pos.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -52,7 +52,9 @@ wlan_wifi_pos_cfg80211_set_wiphy_ext_feature(struct wiphy *wiphy,
 					WLAN_EXT_CAPA11_NTB_RANGING_RESPONDER;
 	}
 
-	if (wlan_psoc_nif_fw_ext2_cap_get(psoc, WLAN_RTT_11AZ_TB_SUPPORT)) {
+	if (wlan_psoc_nif_fw_ext2_cap_get(psoc, WLAN_RTT_11AZ_TB_SUPPORT) ||
+	    wlan_psoc_nif_fw_ext2_cap_get(psoc,
+					  WLAN_RTT_11AZ_TB_RSTA_SUPPORT)) {
 		wlan_extended_caps_iface[WLAN_EXT_RANGING_CAP_IDX] |=
 					WLAN_EXT_CAPA11_TB_RANGING_RESPONDER;
 		wlan_extended_caps_iface_mask[WLAN_EXT_RANGING_CAP_IDX] |=
