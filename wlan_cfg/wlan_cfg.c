8a578fefe14dfcc4592f6ed5cc3ece18cd809bda
Naveen Gurujala <quic_gnaveen@quicinc.com>
2023-04-24 00:35:42 -0700

    Revert "qcacmn: Add check for sawf stats"
    This reverts Change-Id:	Ica417a53e91ebc8415f0f73c3c61b3b35e200de5
    
    Change-Id: I304da02724d3ea97c61511b915a49513e87a55b7
    CRs-Fixed: 3471399

--- wlan_cfg/wlan_cfg.c
+++ wlan_cfg/wlan_cfg.c
@@ -3781,8 +3781,6 @@ uint8_t wlan_cfg_get_sawf_stats_config(struct wlan_cfg_dp_soc_ctxt *cfg)
 	return cfg->sawf_stats;
 }
 
-qdf_export_symbol(wlan_cfg_get_sawf_stats_config);
-
 void wlan_cfg_set_sawf_stats_config(struct wlan_cfg_dp_soc_ctxt *cfg,
 				    uint8_t val)
 {
@@ -3799,8 +3797,6 @@ uint8_t wlan_cfg_get_sawf_stats_config(struct wlan_cfg_dp_soc_ctxt *cfg)
 {
 	return 0;
 }
-
-qdf_export_symbol(wlan_cfg_get_sawf_stats_config);
 
 void wlan_cfg_set_sawf_stats_config(struct wlan_cfg_dp_soc_ctxt *cfg,
 				    uint8_t val)
