075c3ba8894fb0c20beca507057a884d2650eedc
Ripan Deuri <quic_rdeuri@quicinc.com>
2023-05-07 18:29:38 +0530

    qca-wifi: SAWF os_if change to add rules
    Change includes -
    - Define rules attributes need for EPCS.
    - Reuse SCS NL attribute for source mac address.
    - Check bitmask before setting NL attributes.
    - Removed unused NL attributes.
    
    Change-Id: Idff98f7655435416fe9ad2c9474b7c7d1823e8af
    CRs-Fixed: 3519788

--- os_if/linux/sawf/src/wlan_cfg80211_sawf.c
+++ os_if/linux/sawf/src/wlan_cfg80211_sawf.c
@@ -20,12 +20,16 @@
 #include <wlan_cfg80211_sawf.h>
 
 #define SAWF_CFG80211_BUF_LEN 4000
-#define SAWF_SCS_CLASSIFIER_TYPE_DEF 4
-#define SAWF_SCS_TCLAS4_VERSION_DEF 4
+
+/*
+ * Reuse tclas10 filter value for sawf request
+ */
+#define QCA_WLAN_VENDOR_ATTR_SAWF_RULE_SRC_MAC_ADDR \
+	QCA_WLAN_VENDOR_ATTR_SCS_RULE_CONFIG_TCLAS10_FILTER_VALUE
 
 void wlan_cfg80211_sawf_send_rule(struct wiphy *wiphy,
 				  struct wireless_dev *wdev,
-				  struct sawf_rule_config_intf *rule)
+				  struct wlan_sawf_rule *rule)
 {
 	struct sk_buff *skb;
 
@@ -33,8 +37,10 @@ void wlan_cfg80211_sawf_send_rule(struct wiphy *wiphy,
 		(wiphy, wdev, SAWF_CFG80211_BUF_LEN,
 		 QCA_NL80211_VENDOR_SUBCMD_SCS_RULE_CONFIG_INDEX, GFP_ATOMIC);
 
-	if (!skb)
+	if (!skb) {
+		qdf_err("Unable allocate vendor event");
 		return;
+	}
 
 	if (nla_put_u32(skb, QCA_WLAN_VENDOR_ATTR_SCS_RULE_CONFIG_RULE_ID,
 			rule->rule_id)) {
@@ -48,11 +54,9 @@ void wlan_cfg80211_sawf_send_rule(struct wiphy *wiphy,
 		goto fail;
 	}
 
-	if (nla_put_u8(skb,
-		       QCA_WLAN_VENDOR_ATTR_SCS_RULE_CONFIG_CLASSIFIER_TYPE,
-		       SAWF_SCS_CLASSIFIER_TYPE_DEF)) {
-		qdf_err("nla_put fail for classifier_type.");
-		goto fail;
+	if (rule->req_type == WLAN_CFG80211_SAWF_RULE_DELETE) {
+		wlan_cfg80211_vendor_event(skb, GFP_ATOMIC);
+		return;
 	}
 
 	if (nla_put_u16(skb,
@@ -62,22 +66,20 @@ void wlan_cfg80211_sawf_send_rule(struct wiphy *wiphy,
 		goto fail;
 	}
 
-	if (nla_put_u8(skb, QCA_WLAN_VENDOR_ATTR_SCS_RULE_CONFIG_OUTPUT_TID,
-		       rule->output_tid)) {
-		qdf_err("nla_put fail for output tid.");
-		goto fail;
+	if (qdf_test_bit(SAWF_RULE_IN_PARAM_DST_MAC, rule->param_bitmap)) {
+		if (nla_put(skb, QCA_WLAN_VENDOR_ATTR_SCS_RULE_CONFIG_DST_MAC_ADDR,
+			    QDF_MAC_ADDR_SIZE, rule->dst_mac)) {
+			qdf_err("nla_put fail for dst_mac_addr.") ;
+			goto fail;
+		}
 	}
 
-	if (nla_put_u8(skb, QCA_WLAN_VENDOR_ATTR_SCS_RULE_CONFIG_TCLAS4_VERSION,
-		       SAWF_SCS_TCLAS4_VERSION_DEF)) {
-		qdf_err("nla_put fail for tclas4 version.");
-		goto fail;
-	}
-
-	if (nla_put(skb, QCA_WLAN_VENDOR_ATTR_SCS_RULE_CONFIG_DST_MAC_ADDR,
-		    QDF_MAC_ADDR_SIZE, rule->dst_mac)) {
-		qdf_err("nla_put fail for dst_mac_addr.") ;
-		goto fail;
+	if (qdf_test_bit(SAWF_RULE_IN_PARAM_SRC_MAC, rule->param_bitmap)) {
+		if (nla_put(skb, QCA_WLAN_VENDOR_ATTR_SAWF_RULE_SRC_MAC_ADDR,
+			    QDF_MAC_ADDR_SIZE, rule->src_mac)) {
+			qdf_err("nla_put fail for src_mac_addr.") ;
+			goto fail;
+		}
 	}
 
 	wlan_cfg80211_vendor_event(skb, GFP_ATOMIC);
