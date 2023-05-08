c3a34a4987f408c28d18692e74a7a6b4eb82b965
Vijay Patil <quic_vijaylil@quicinc.com>
2023-05-08 21:01:42 +0530

    qcacmn: Iterate through all vdevs and check if CAC is in progress
    Add an API to iterate through all vdevs and check if any vdev is
    in CAC in-progress state.
    
    When DFS CAC is going on for SAP, if 5 GHz mac is occupied by the
    SAP and it's not available for 5 GHz NAN discovery. Enabling NAN
    on 5 GHz in such cases cause inconsistency in firmware state machine
    and firmware may assert as DFS MAC is not supposed to be disturbed
    while CAC is going on.
    
    So, check if NAN enable request has 5 GHz NAN channel also and reject
    the request. Allow NAN enable command if it's 2 GHz only NAN enable
    request, while CAC is going on.
    
    Change-Id: Idcd92e29d4b6078332d439efb75eba6800e986bb
    CRs-Fixed: 3492054

--- umac/cmn_services/utils/inc/wlan_utility.h
+++ umac/cmn_services/utils/inc/wlan_utility.h
@@ -1044,4 +1044,15 @@ void wlan_minidump_remove(void *start_addr, const size_t size,
 			  enum wlan_minidump_host_data type,
 			  const char *name);
 
+/**
+ * wlan_util_is_vdev_in_cac_wait() - Check if dfs sap vdev is in cac wait
+ * @pdev: pdev object
+ * @dbg_id: object manager ref id
+ *
+ * This function checks if dfs sap vdev is in cac wait state
+ *
+ * Return: true, if cac is in progress, otherwise false
+ */
+bool wlan_util_is_vdev_in_cac_wait(struct wlan_objmgr_pdev *pdev,
+				   wlan_objmgr_ref_dbgid dbg_id);
 #endif /* _WLAN_UTILITY_H_ */
