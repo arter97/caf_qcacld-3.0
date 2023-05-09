b4c77ab9245bf262b650c28e4ae44f79cfd87d2d
Vijay Raj <quic_vijaraj@quicinc.com>
2023-05-09 04:24:23 -0700

    qcacmn: Add structure support for vlan peer config
    Add structure peer_vlan_config_param for
    WMI_PEER_CONFIG_VLAN_CMDID to support vlan peer config
    
    Change-Id: I2f93ce2bd10d1920d33ee0e26d1b7d344813852b
    CRs-Fixed: 3492156

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -9392,6 +9392,36 @@ struct vap_tidmap_prec_params {
 #endif
 
 /**
+ * struct peer_vlan_config_param - peer vlan parameter
+ * @tx_cmd: Tx command
+ * @rx_cmd: Rx Command
+ * @tx_strip_insert: Strip or Insert vlan in Tx[0:Strip, 1: Insert]
+ * @tx_strip_insert_inner: Enable tx_strip_insert operation for inner vlan tag.
+ * @tx_strip_insert_outer: Enable tx_strip_insert operation for outer vlan tag.
+ * @rx_strip_c_tag: Strip c_tag
+ * @rx_strip_s_tag: Strip s_tag
+ * @rx_insert_c_tag: Insert c_tag
+ * @rx_insert_s_tag: Insert s_tag
+ * @insert_vlan_inner_tci: Vlan inner tci
+ * @insert_vlan_outer_tci: Vlan outer tci
+ * @vdev_id: vdev id corresponding to peer.
+ */
+struct peer_vlan_config_param {
+	uint16_t tx_cmd:1;
+	uint16_t rx_cmd:1;
+	uint16_t tx_strip_insert:1;
+	uint16_t tx_strip_insert_inner:1;
+	uint16_t tx_strip_insert_outer:1;
+	uint16_t rx_strip_c_tag:1;
+	uint16_t rx_strip_s_tag:1;
+	uint16_t rx_insert_c_tag:1;
+	uint16_t rx_insert_s_tag:1;
+	uint16_t insert_vlan_inner_tci;
+	uint16_t insert_vlan_outer_tci;
+	uint8_t vdev_id;
+};
+
+/**
  * struct wmi_cfr_peer_tx_event_param - CFR peer tx_event params
  * @capture_method: CFR data capture method
  * @vdev_id: ID of vdev to which this info belongs
