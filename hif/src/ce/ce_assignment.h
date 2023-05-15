629e6d0fbeba840345451a86ab26e1c9f9003c91
Manikanta Pubbisetty <quic_mpubbise@quicinc.com>
2023-05-15 14:48:14 +0530

    qcacmn: Do not allocate RX buffers for CE5 if PKTLOG disabled
    Currently for WCN6750, RX buffers for CE5 (used for PKTLOG) are
    allocated although PKTLOG code is not compiled into the driver.
    This leads to wastage of memory. If allocations are done
    conditionally, then a memory of 2MB will be saved when PKTLOG
    feature is not compiled.
    
    Change-Id: Id6c43cd0a1d15d3553e1e37910b067ef705b15f6
    CRs-Fixed: 3506469

--- hif/src/ce/ce_assignment.h
+++ hif/src/ce/ce_assignment.h
@@ -1588,8 +1588,12 @@ static struct CE_attr host_ce_config_wlan_qca6750[] = {
 	{ /* CE4 */ CE4_COMP_HTT_HTC, 0,
 		CE_HTT_H2T_MSG_SRC_NENTRIES, 256, 0, NULL,},
 	/* target -> host PKTLOG */
+#ifdef REMOVE_PKT_LOG
+	{ /* CE5 */ 0, 0, 0, 0, 0, NULL,},
+#else
 	{ /* CE5 */ (CE_ATTR_FLAGS | CE_ATTR_DISABLE_INTR), 0, 0, 2048, 512,
 		NULL,},
+#endif
 	/* Target autonomous HIF_memcpy */
 	{ /* CE6 */ CE_ATTR_FLAGS, 0, 0, 0, 0, NULL,},
 #ifdef WLAN_FEATURE_WMI_DIAG_OVER_CE7
@@ -1618,7 +1622,11 @@ static struct CE_pipe_config target_ce_config_wlan_qca6750[] = {
 	{ /* CE4 */ 4, PIPEDIR_OUT, 256, 256,
 		(CE_ATTR_FLAGS | CE_ATTR_DISABLE_INTR), 0,},
 	/* Target -> host PKTLOG */
+#ifdef REMOVE_PKT_LOG
+	{ /* CE5 */ 5, PIPEDIR_IN,  2, 2048, CE_ATTR_FLAGS, 0,},
+#else
 	{ /* CE5 */ 5, PIPEDIR_IN,  32, 2048, CE_ATTR_FLAGS, 0,},
+#endif
 	/* Reserved for target autonomous HIF_memcpy */
 	{ /* CE6 */ 6, PIPEDIR_INOUT, 32, 16384, CE_ATTR_FLAGS, 0,},
 #ifdef WLAN_FEATURE_WMI_DIAG_OVER_CE7
