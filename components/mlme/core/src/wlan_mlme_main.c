539c25dc9755920fde081c94ba423312f6b04ed8
Jianmin Zhu <quic_jianminz@quicinc.com>
2023-06-04 08:27:37 +0800

    qcacld-3.0: Use ini to set mlo link band
    Force single link on 2 GHz:
    mlo_support_link_num=1, mlo_support_link_band=1
    
    Force single link on 5 GHz:
    mlo_support_link_num=1, mlo_support_link_band=2
    
    Force single link on 6 GHz:
    mlo_support_link_num=1, mlo_support_link_band=4
    
    Force 2 links on 2+5 GHz, force assoc link 2 GHz:
    mlo_support_link_num=2, mlo_support_link_band=0x13
    
    Force 2 links on 2+5 GHz, force assoc link 5 GHz:
    mlo_support_link_num=2, mlo_support_link_band=0x23
    
    Force 2 links on 5+6 GHz, force assoc link 6 GHz:
    mlo_support_link_num=2, mlo_support_link_band=0x46
    
    Change-Id: Id6f56421528a42aa7059693845fe71a206bded93
    CRs-Fixed: 3722009

--- components/mlme/core/src/wlan_mlme_main.c
+++ components/mlme/core/src/wlan_mlme_main.c
@@ -2575,7 +2575,7 @@ static void mlme_init_sta_mlo_cfg(struct wlan_objmgr_psoc *psoc,
 	sta->mlo_support_link_num =
 		cfg_get(psoc, CFG_MLO_SUPPORT_LINK_NUM);
 	sta->mlo_support_link_band =
-		cfg_default(CFG_MLO_SUPPORT_LINK_BAND);
+		cfg_get(psoc, CFG_MLO_SUPPORT_LINK_BAND);
 	sta->mlo_max_simultaneous_links =
 		cfg_default(CFG_MLO_MAX_SIMULTANEOUS_LINKS);
 	sta->mlo_prefer_percentage =
@@ -2584,6 +2584,8 @@ static void mlme_init_sta_mlo_cfg(struct wlan_objmgr_psoc *psoc,
 		cfg_default(CFG_MLO_SAME_LINK_MLD_ADDR);
 	sta->mlo_5gl_5gh_mlsr =
 		cfg_default(CFG_MLO_MLO_5GL_5GH_MLSR);
+	mlme_debug("mlo_support_link_num: %d, mlo_support_link_band: 0x%x",
+		   sta->mlo_support_link_num, sta->mlo_support_link_band);
 }
 
 static bool
--- components/mlme/dispatcher/inc/cfg_mlme_sta.h
+++ components/mlme/dispatcher/inc/cfg_mlme_sta.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -648,7 +648,7 @@
  *
  * </cfg>
  */
-#define CFG_MLO_SUPPORT_LINK_BAND CFG_UINT( \
+#define CFG_MLO_SUPPORT_LINK_BAND CFG_INI_UINT( \
 			"mlo_support_link_band", \
 			0x1, \
 			0x77, \
