f0348b8522a207808f9e1b83d24b621c0ff2af87
Venkateswara Naralasetty <quic_vnaralas@quicinc.com>
2023-06-05 10:43:20 +0530

    qcacmn: update wcn6450 hw srng table for CFR support
    Add CFR related rings configuration to wcn6450 hw srng table.
    
    Change-Id: I5088d5ba7759d96b64319ae6f1ee26255415b900
    CRs-Fixed: 3519261

--- hal/wifi3.0/wcn6450/hal_wcn6450.c
+++ hal/wifi3.0/wcn6450/hal_wcn6450.c
@@ -175,9 +175,37 @@ struct hal_hw_srng_config hw_srng_table_wcn6450[] = {
 	},
 	{/* RXDMA_MONITOR_DST */ 0},
 	{/* RXDMA_MONITOR_DESC */ 0},
-	{/* DIR_BUF_RX_DMA_SRC */ 0},
+	{/* DIR_BUF_RX_DMA_SRC */
+		.start_ring_id = HAL_SRNG_DIR_BUF_RX_SRC_DMA_RING,
+		/*
+		 * one ring is for spectral scan
+		 * the other is for cfr
+		 */
+		.max_rings = 2,
+		.entry_size = 2,
+		.lmac_ring = TRUE,
+		.ring_dir = HAL_SRNG_SRC_RING,
+		/* reg_start is not set because LMAC rings are not accessed
+		 * from host
+		 */
+		.reg_start = {},
+		.reg_size = {},
+		.max_size = HAL_RXDMA_MAX_RING_SIZE,
+	},
 #ifdef WLAN_FEATURE_CIF_CFR
-	{/* WIFI_POS_SRC */ 0},
+	{/* WIFI_POS_SRC */
+		.start_ring_id = HAL_SRNG_WIFI_POS_SRC_DMA_RING,
+		.max_rings = 1,
+		.entry_size = sizeof(wmi_oem_dma_buf_release_entry)  >> 2,
+		.lmac_ring = TRUE,
+		.ring_dir = HAL_SRNG_SRC_RING,
+		/* reg_start is not set because LMAC rings are not accessed
+		 * from host
+		 */
+		.reg_start = {},
+		.reg_size = {},
+		.max_size = HAL_RXDMA_MAX_RING_SIZE,
+	},
 #endif
 	{ /* REO2PPE */ 0},
 	{ /* PPE2TCL */ 0},
