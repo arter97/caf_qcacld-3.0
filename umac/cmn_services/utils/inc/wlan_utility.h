2b0b767e60e7bf912ae5c02ea7400eef9250ddd9
Venkateswara Naralasetty <quic_vnaralas@quicinc.com>
2023-06-09 10:27:45 +0530

    qcacmn: Add a new enum for SW2RXDMA_LINK_RING
    Add a new enum for SW2RXDMA_LINK_RING which is used to
    handover the links to HW in monitor mode for wcn6450.
    
    Change-Id: I0a3cd19f9891ae323742fd8bebfca5ff20b14b20
    CRs-Fixed: 3525001

--- umac/cmn_services/utils/inc/wlan_utility.h
+++ umac/cmn_services/utils/inc/wlan_utility.h
@@ -974,6 +974,7 @@ uint16_t wlan_util_get_peer_count_for_mode(struct wlan_objmgr_pdev *pdev,
  * @WLAN_MD_DP_SRNG_PPE2TCL: dp_srng type for PPE tx ring
  * @WLAN_MD_DP_SRNG_PPE_RELEASE: dp_srng type for PPE tx com ring
  * @WLAN_MD_DP_SRNG_PPE_WBM2SW_RELEASE: dp_srng type for PPE2TCL tx com ring
+ * @WLAN_MD_DP_SRNG_SW2RXDMA_LINK_RING: dp_srng type for SW2RXDMA link ring
  * @WLAN_MD_MAX: Max value
  */
 enum wlan_minidump_host_data {
@@ -1015,6 +1016,7 @@ enum wlan_minidump_host_data {
 	WLAN_MD_DP_SRNG_PPE2TCL,
 	WLAN_MD_DP_SRNG_PPE_RELEASE,
 	WLAN_MD_DP_SRNG_PPE_WBM2SW_RELEASE,
+	WLAN_MD_DP_SRNG_SW2RXDMA_LINK_RING,
 	WLAN_MD_MAX
 };
 
