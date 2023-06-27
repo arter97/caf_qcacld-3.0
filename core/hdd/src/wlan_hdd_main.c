6743d7d156e93bc61a92d18fee21d6c0b5702695
Liangwei Dong <quic_liangwei@quicinc.com>
2023-06-27 13:37:57 +0800

    qcacld-3.0: Don't set mac address if p2p device is up
    The current dynamic mac address updating feature doesn't
    support p2p device which require self peer delete and create
    without vdev delete and create.
    Reject the set mac address if p2p interface is up to avoid
    stability issue.
    
    Change-Id: I8a9c60f77e227c44580ec15a3863c36f2f09a8e5
    CRs-Fixed: 3543158

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -5649,8 +5649,6 @@ bool hdd_is_dynamic_set_mac_addr_allowed(struct hdd_adapter *adapter)
 			hdd_info_rl("VDEV is not in disconnected state, set mac address isn't supported");
 			return false;
 		}
-	fallthrough;
-	case QDF_P2P_DEVICE_MODE:
 		return true;
 	case QDF_SAP_MODE:
 		if (test_bit(SOFTAP_BSS_STARTED,
