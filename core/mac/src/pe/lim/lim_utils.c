ef9686e9e82c0626471192771210b08131259e6e
Arun Kumar Khandavalli <quic_akhandav@quicinc.com>
2023-06-30 13:55:58 +0530

    qcacld-3.0: Update the 11be draft version
    Now that all the draft 3.0 version related changes are merged update
    the draft version accordingly.
    
    Change-Id: If6a4d1e1cef8923af03d321229bba0443f37103a
    CRs-Fixed: 3545914

--- core/mac/src/pe/lim/lim_utils.c
+++ core/mac/src/pe/lim/lim_utils.c
@@ -8762,7 +8762,7 @@ void lim_update_session_eht_capable(struct mac_context *mac,
 {
 	session->eht_capable = true;
 	pe_debug("eht_capable: %d", session->eht_capable);
-	pe_debug("Draft 2.0 support enabled");
+	pe_debug("Draft 3.0 support enabled");
 }
 
 void lim_add_bss_eht_cfg(struct bss_params *add_bss, struct pe_session *session)
