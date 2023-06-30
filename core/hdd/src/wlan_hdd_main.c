aae436187a984e0f9b23bb1387d4e2f468f6686d
Zhiwei Yang <quic_zhiwyang@quicinc.com>
2023-06-30 15:29:45 +0800

    qcacld-3.0: remove hif_ce_desc_history from hdd
    move hif_ce_desc_history_log_register to hif_open and move
    hif_ce_desc_history_log_unregister to hif_close is more suitable.
    
    Change-Id: Iab99ef119c717aba2de46d1f2731d9cb4131b761
    CRs-Fixed: 3549458

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -242,7 +242,6 @@
 #include "wlan_psoc_mlme_ucfg_api.h"
 #include "os_if_qmi.h"
 #include "wlan_qmi_ucfg_api.h"
-#include "ce_api.h"
 #include "wlan_psoc_mlme_ucfg_api.h"
 #include "wlan_ll_sap_ucfg_api.h"
 
@@ -4823,8 +4822,6 @@ int hdd_wlan_start_modules(struct hdd_context *hdd_ctx, bool reinit)
 			hdd_err("Failed to open hif; errno: %d", ret);
 			goto power_down;
 		}
-
-		hif_ce_desc_history_log_register();
 
 		hif_ctx = cds_get_context(QDF_MODULE_ID_HIF);
 		if (!hif_ctx) {
@@ -16213,8 +16210,6 @@ int hdd_wlan_stop_modules(struct hdd_context *hdd_ctx, bool ftm_mode)
 	wlan_connectivity_logging_stop();
 
 	ucfg_ipa_component_config_free();
-
-	hif_ce_desc_history_log_unregister();
 
 	hdd_hif_close(hdd_ctx, hif_ctx);
 
