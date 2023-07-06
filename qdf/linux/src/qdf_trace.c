633e9c878b83ae4488c0c15e6ce6619f3cc0ff8d
Venkateswara Swamy Bandaru <quic_vbandaru@quicinc.com>
2023-07-06 14:52:05 +0530

    qcacmn: Add EPCS module id
    Add EPCS module id and initialize to default info
    debug level.
    
    Change-Id: Idec60fcab1bdef90660a24c54f6c7b09d0ddc2eb
    CRs-Fixed: 3490401

--- qdf/linux/src/qdf_trace.c
+++ qdf/linux/src/qdf_trace.c
@@ -3427,6 +3427,7 @@ struct category_name_info g_qdf_category_name[MAX_SUPPORTED_CATEGORY] = {
 	[QDF_MODULE_ID_WIFI_RADAR] = {"WIFI RADAR"},
 	[QDF_MODULE_ID_CDP] =  {"CDP"},
 	[QDF_MODULE_ID_QMI] = {"QMI"},
+	[QDF_MODULE_ID_EPCS] = {"EPCS"},
 	[QDF_MODULE_ID_ANY] = {"ANY"},
 };
 qdf_export_symbol(g_qdf_category_name);
@@ -3998,6 +3999,7 @@ static void set_default_trace_levels(struct category_info *cinfo)
 		[QDF_MODULE_ID_WIFI_RADAR] = QDF_TRACE_LEVEL_NONE,
 		[QDF_MODULE_ID_TARGET] = QDF_TRACE_LEVEL_NONE,
 		[QDF_MODULE_ID_QMI] = QDF_TRACE_LEVEL_ERROR,
+		[QDF_MODULE_ID_EPCS] = QDF_TRACE_LEVEL_INFO,
 		[QDF_MODULE_ID_ANY] = QDF_TRACE_LEVEL_INFO,
 	};
 
