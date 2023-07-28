1bb149d536c8f1eb5d448f9bd16e6e44653d2180
Guru Pratap Sharma <quic_guruprat@quicinc.com>
2023-07-27 22:02:40 -0700

    qcacmn: Adding WLAN_VDEV_RATEMASK_TYPE_EHT in vdev ratemask type
    Adding WLAN_VDEV_RATEMASK_TYPE_EHT in vdev ratemask type enum
    
    Change-Id: I3a75b02eb059bd8c7f504ce46685f9c038a75c82
    CRs-Fixed: 3573994

--- umac/mlme/include/wlan_vdev_mlme.h
+++ umac/mlme/include/wlan_vdev_mlme.h
@@ -486,6 +486,7 @@ struct vdev_mlme_inactivity_params {
  * @WLAN_VDEV_RATEMASK_TYPE_HT: phy type ht
  * @WLAN_VDEV_RATEMASK_TYPE_VHT: phy type vht
  * @WLAN_VDEV_RATEMASK_TYPE_HE: phy type he
+ * @WLAN_VDEV_RATEMASK_TYPE_EHT: phy type eht
  * @WLAN_VDEV_RATEMASK_TYPE_MAX: Maximum enumeration
  */
 enum vdev_ratemask_type {
@@ -493,6 +494,7 @@ enum vdev_ratemask_type {
 	WLAN_VDEV_RATEMASK_TYPE_HT,
 	WLAN_VDEV_RATEMASK_TYPE_VHT,
 	WLAN_VDEV_RATEMASK_TYPE_HE,
+	WLAN_VDEV_RATEMASK_TYPE_EHT,
 	WLAN_VDEV_RATEMASK_TYPE_MAX,
 };
 
