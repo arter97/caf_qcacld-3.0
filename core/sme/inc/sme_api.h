a14b68755bbb36d8f1ecefad05be24e9ff999754
Gururaj Pandurangi <quic_panduran@quicinc.com>
2023-08-29 00:37:36 -0700

    qcacld-3.0: Configure BA mode on each ML STA links
    Currently, when the CAPI to set Manual BA mode is received
    Host configures assoc vdev and sends it to FW, where as this
    value is stored in mac context and sent to FW during vdev
    start response for partner vdev. FW honors Host input during
    peer create that happens before vdev start and thus, the BA
    mode is not updated for partner vdev. This leads to FW
    sending undesired addba request to AP. Thus, configure BA
    mode to Manual on both vdevs immediately upon receiving
    input from user space.
    
    Change-Id: Id71c2e3b8fa523af94773b6346fd023784e0d85a
    CRs-Fixed: 3584312

--- core/sme/inc/sme_api.h
+++ core/sme/inc/sme_api.h
@@ -3058,17 +3058,6 @@ int sme_set_auto_rate_ldpc(mac_handle_t mac_handle, uint8_t session_id,
 int sme_set_auto_rate_he_ltf(mac_handle_t mac_handle, uint8_t session_id,
 			     uint8_t cfg_val);
 
-/**
- * sme_set_ba_opmode() - sets the BA op mode
- * @mac_handle: Opaque handle to the global MAC context
- * @session_id: SME session id
- * @cfg_val: BA mode
- *
- * Return: None
- */
-void sme_set_ba_opmode(mac_handle_t mac_handle, uint8_t session_id,
-		       bool cfg_val);
-
 #ifdef WLAN_FEATURE_11BE
 /**
  * sme_update_tgt_eht_cap() - sets the EHT caps to pmac
@@ -3813,6 +3802,15 @@ int sme_update_eht_caps(mac_handle_t mac_handle, uint8_t session_id,
 int sme_send_vdev_pause_for_bcn_period(mac_handle_t mac_handle,
 				       uint8_t session_id,
 				       uint8_t cfg_val);
+
+/**
+ * sme_set_per_link_ba_mode() - sets BA mode for each STA MLD link
+ * @mac_handle: Opaque handle to the global MAC context
+ * @val: BA mode
+ *
+ * Return: None
+ */
+void sme_set_per_link_ba_mode(mac_handle_t mac_handle, uint8_t val);
 #else
 static inline void sme_set_eht_testbed_def(mac_handle_t mac_handle,
 					   uint8_t vdev_id)
@@ -3864,6 +3862,10 @@ void sme_activate_mlo_links(mac_handle_t mac_handle, uint8_t session_id,
 			    struct qdf_mac_addr active_link_addr[2])
 {
 }
+
+static inline
+void sme_set_per_link_ba_mode(mac_handle_t mac_handle, uint8_t val)
+{}
 #endif
 
 /**
