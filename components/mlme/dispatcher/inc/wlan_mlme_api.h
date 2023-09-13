440806f251b26d439bc1648696433fcb642ec7da
Aasir Rasheed <quic_arasheed@quicinc.com>
2023-09-13 16:19:28 +0530

    qcacld-3.0: Notify firmware on CSA rejection
    The host driver now sends a notification to the
    firmware when a CSA (Channel Switch Announcement)
    is rejected due to no change in channel, bandwidth,
    or puncture.
    
    Change-Id: I5d8a424459898d15a7931baf7ca4c3de8308c64e
    CRs-Fixed: 3595843

--- components/mlme/dispatcher/inc/wlan_mlme_api.h
+++ components/mlme/dispatcher/inc/wlan_mlme_api.h
@@ -4345,4 +4345,15 @@ enum phy_ch_width wlan_mlme_get_max_bw(void);
  */
 QDF_STATUS wlan_mlme_get_sta_ch_width(struct wlan_objmgr_vdev *vdev,
 				      enum phy_ch_width *ch_width);
+
+/**
+ * wlan_mlme_send_csa_event_status_ind() - send csa event status ind
+ * @vdev: vdev obj
+ * @csa_status: csa status
+ *
+ * Return: QDF_STATUS
+ */
+QDF_STATUS
+wlan_mlme_send_csa_event_status_ind(struct wlan_objmgr_vdev *vdev,
+				    uint8_t csa_status);
 #endif /* _WLAN_MLME_API_H_ */
