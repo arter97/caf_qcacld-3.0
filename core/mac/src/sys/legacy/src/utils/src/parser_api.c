c6112edbb859a362eff8c3ed5154c3195616bd50
Ke Huang <quic_keh@quicinc.com>
2023-09-15 11:48:45 +0530

    qcacld-3.0: Fix wrong error code for Association Response
    The Association Response with wrong status code(0x01: Unspecified
    failure) to Association Request without rates IE.
    
    Change to send the Association Response with status code 0x12(due
    to requesting STA not supporting all of the data rates in the
    BSSBasicRateSet) to deny this Association Request without rates
    IE.
    
    Change-Id: I16cc4d54fa62530bb049e4f3151ed22814a52b4b
    CRs-Fixed: 3617022

--- core/mac/src/sys/legacy/src/utils/src/parser_api.c
+++ core/mac/src/sys/legacy/src/utils/src/parser_api.c
@@ -3308,7 +3308,7 @@ sir_convert_assoc_req_frame2_mlo_struct(uint8_t *pFrame,
 }
 #endif
 
-QDF_STATUS
+enum wlan_status_code
 sir_convert_assoc_req_frame2_struct(struct mac_context *mac,
 				    uint8_t *pFrame,
 				    uint32_t nFrame, tpSirAssocReq pAssocReq)
@@ -3318,7 +3318,7 @@ sir_convert_assoc_req_frame2_struct(struct mac_context *mac,
 
 	ar = qdf_mem_malloc(sizeof(tDot11fAssocRequest));
 	if (!ar)
-		return QDF_STATUS_E_NOMEM;
+		return STATUS_UNSPECIFIED_FAILURE;
 
 	/* delegate to the framesc-generated code, */
 	status = dot11f_unpack_assoc_request(mac, pFrame, nFrame, ar, false);
@@ -3328,7 +3328,7 @@ sir_convert_assoc_req_frame2_struct(struct mac_context *mac,
 		QDF_TRACE_HEX_DUMP(QDF_MODULE_ID_PE, QDF_TRACE_LEVEL_ERROR,
 				   pFrame, nFrame);
 		qdf_mem_free(ar);
-		return QDF_STATUS_E_FAILURE;
+		return STATUS_UNSPECIFIED_FAILURE;
 	} else if (DOT11F_WARNED(status)) {
 		pe_debug("There were warnings while unpacking an Association Request (0x%08x, %d bytes):",
 			status, nFrame);
@@ -3450,13 +3450,13 @@ sir_convert_assoc_req_frame2_struct(struct mac_context *mac,
 	if (!pAssocReq->ssidPresent) {
 		pe_debug("Received Assoc without SSID IE");
 		qdf_mem_free(ar);
-		return QDF_STATUS_E_FAILURE;
+		return STATUS_UNSPECIFIED_FAILURE;
 	}
 
 	if (!pAssocReq->suppRatesPresent && !pAssocReq->extendedRatesPresent) {
 		pe_debug("Received Assoc without supp rate IE");
 		qdf_mem_free(ar);
-		return QDF_STATUS_E_FAILURE;
+		return STATUS_ASSOC_DENIED_RATES;
 	}
 	if (ar->VHTCaps.present) {
 		qdf_mem_copy(&pAssocReq->VHTCaps, &ar->VHTCaps,
@@ -3526,7 +3526,7 @@ sir_convert_assoc_req_frame2_struct(struct mac_context *mac,
 		 ar->eht_cap.present);
 
 	qdf_mem_free(ar);
-	return QDF_STATUS_SUCCESS;
+	return STATUS_SUCCESS;
 
 } /* End sir_convert_assoc_req_frame2_struct. */
 
@@ -4343,7 +4343,7 @@ sir_convert_reassoc_req_frame2_mlo_struct(uint8_t *pframe, uint32_t nframe,
 	return QDF_STATUS_SUCCESS;
 }
 #endif
-QDF_STATUS
+enum wlan_status_code
 sir_convert_reassoc_req_frame2_struct(struct mac_context *mac,
 				      uint8_t *pFrame,
 				      uint32_t nFrame, tpSirAssocReq pAssocReq)
@@ -4353,7 +4353,7 @@ sir_convert_reassoc_req_frame2_struct(struct mac_context *mac,
 
 	ar = qdf_mem_malloc(sizeof(*ar));
 	if (!ar)
-		return QDF_STATUS_E_NOMEM;
+		return STATUS_UNSPECIFIED_FAILURE;
 
 	/* delegate to the framesc-generated code, */
 	status = dot11f_unpack_re_assoc_request(mac, pFrame, nFrame,
@@ -4364,7 +4364,7 @@ sir_convert_reassoc_req_frame2_struct(struct mac_context *mac,
 		QDF_TRACE_HEX_DUMP(QDF_MODULE_ID_PE, QDF_TRACE_LEVEL_ERROR,
 				   pFrame, nFrame);
 		qdf_mem_free(ar);
-		return QDF_STATUS_E_FAILURE;
+		return STATUS_UNSPECIFIED_FAILURE;
 	} else if (DOT11F_WARNED(status)) {
 		pe_debug("There were warnings while unpacking a Re-association Request (0x%08x, %d bytes):",
 			status, nFrame);
@@ -4462,13 +4462,13 @@ sir_convert_reassoc_req_frame2_struct(struct mac_context *mac,
 	if (!pAssocReq->ssidPresent) {
 		pe_debug("Received Assoc without SSID IE");
 		qdf_mem_free(ar);
-		return QDF_STATUS_E_FAILURE;
+		return STATUS_UNSPECIFIED_FAILURE;
 	}
 
 	if (!pAssocReq->suppRatesPresent && !pAssocReq->extendedRatesPresent) {
 		pe_debug("Received Assoc without supp rate IE");
 		qdf_mem_free(ar);
-		return QDF_STATUS_E_FAILURE;
+		return STATUS_ASSOC_DENIED_RATES;
 	}
 	/* Why no call to 'updateAssocReqFromPropCapability' here, like */
 	/* there is in 'sir_convert_assoc_req_frame2_struct'? */
@@ -4537,7 +4537,7 @@ sir_convert_reassoc_req_frame2_struct(struct mac_context *mac,
 						  ar, pAssocReq);
 	qdf_mem_free(ar);
 
-	return QDF_STATUS_SUCCESS;
+	return STATUS_SUCCESS;
 
 } /* End sir_convert_reassoc_req_frame2_struct. */
 
