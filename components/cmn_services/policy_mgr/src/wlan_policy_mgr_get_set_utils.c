25f3d71384f1d1e340fdc843ff112ca5b3acac2a
Jyoti Kumari <quic_jyotkuma@quicinc.com>
2023-10-18 00:32:02 +0530

    qcacld-3.0: Give preference to 5 GHz low freq in ACS
    For LL_LT_SAP, give preference to 5 GHz low frequency to select
    channel during ACS.
    
    In case of SBS, it's better to bring up LL_LT_SAP on 5 GHz low
    as there will be less number of passive channels on 5 GHz low
    band compared to 5 GHz high band.
    
    Change-Id: I1d7f7c08c315394315df3b360c9c632d17a4b39a
    CRs-Fixed: 3648218

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_get_set_utils.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_get_set_utils.c
@@ -12684,3 +12684,18 @@ policy_mgr_get_connection_max_channel_width(struct wlan_objmgr_psoc *psoc)
 	return bw;
 }
 
+bool policy_mgr_is_given_freq_5g_low(struct wlan_objmgr_psoc *psoc,
+				     qdf_freq_t given_freq)
+{
+	qdf_freq_t sbs_cut_off_freq;
+
+	sbs_cut_off_freq = policy_mgr_get_sbs_cut_off_freq(psoc);
+	if (!sbs_cut_off_freq)
+		return false;
+
+	if (given_freq < sbs_cut_off_freq &&
+	    WLAN_REG_IS_5GHZ_CH_FREQ(given_freq))
+		return true;
+
+	return false;
+}
