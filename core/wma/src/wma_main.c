4146094938ae5e2d619165ffdc8a234c2b7ec164
Nijun Gong <quic_ngong@quicinc.com>
2023-11-13 12:44:35 +0800

    qcacld-3.0: enum is not match
    wmi_host_hw_mode_config_type is not match with
    wlan_mlme_hw_mode_config_type, causing comparison error.
    
    Change-Id: I10f4e7d283a96b8a227c73aa150bac86191fbee7
    CRs-Fixed: 3662376

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -6215,8 +6215,10 @@ static void wma_update_mlme_aux_dev_caps(struct wlan_objmgr_psoc *psoc,
 	enum wmi_host_hw_mode_config_type hw_mode_id;
 	struct wlan_mlme_aux_dev_caps
 		wlan_mlme_aux0_dev_caps[WLAN_MLME_HW_MODE_MAX] = {0};
+	enum wmi_host_hw_mode_config_type mode;
 
-	if (WMI_HOST_HW_MODE_MAX != WLAN_MLME_HW_MODE_MAX)
+	mode = (enum wmi_host_hw_mode_config_type)WLAN_MLME_HW_MODE_MAX;
+	if (WMI_HOST_HW_MODE_MAX != mode)
 		wma_err("struct define mismatch, pls fix it.");
 
 	num_aux_dev_caps =
