3220df6df723a50f0e494a2bcf60d2bf0932eeed
Yu Wang <quic_yyuwang@quicinc.com>
2023-11-21 18:22:13 +0800

    qcacld-3.0: refine the code for channel load request processing
    Handle subelement Bandwidth-Indication and Wide-Bandwidth-Channel-Switch
    separately when processing channel load request.
    Fix some 'might-be-used-uninitialized' issues.
    
    Change-Id: I8cb7cebc5ab3c9521693672e94180f5fec5e84a1
    CRs-Fixed: 3669137

--- core/mac/src/pe/rrm/rrm_api.c
+++ core/mac/src/pe/rrm/rrm_api.c
@@ -2061,8 +2061,8 @@ rrm_process_channel_load_req(struct mac_context *mac,
 {
 	struct scheduler_msg msg = {0};
 	struct ch_load_ind *load_ind;
-	struct bw_ind_element bw_ind;
-	struct wide_bw_chan_switch wide_bw;
+	struct bw_ind_element bw_ind = {0};
+	struct wide_bw_chan_switch wide_bw = {0};
 	struct rrm_reporting rrm_report;
 	uint8_t op_class, channel;
 	uint16_t randomization_intv, meas_duration, max_meas_duration;
@@ -2110,7 +2110,10 @@ rrm_process_channel_load_req(struct mac_context *mac,
 			pe_debug("Dropping req: invalid is_bw_ind_element IE");
 			return eRRM_REFUSED;
 		}
-	} else if (is_wide_bw_chan_switch) {
+	}
+
+	if (is_wide_bw_chan_switch) {
+		wide_bw.is_wide_bw_chan_switch = true;
 		wide_bw.channel_width = chan_load_req->measurement_request.channel_load.wide_bw_chan_switch.new_chan_width;
 		wide_bw.center_chan_freq0 = chan_load_req->measurement_request.channel_load.wide_bw_chan_switch.new_center_chan_freq0;
 		wide_bw.center_chan_freq1 = chan_load_req->measurement_request.channel_load.wide_bw_chan_switch.new_center_chan_freq1;
@@ -2118,12 +2121,10 @@ rrm_process_channel_load_req(struct mac_context *mac,
 			 wide_bw.channel_width, wide_bw.center_chan_freq0,
 			 wide_bw.center_chan_freq1);
 		if (wide_bw.channel_width < CH_WIDTH_20MHZ ||
-		    bw_ind.channel_width >= CH_WIDTH_320MHZ) {
+		    wide_bw.channel_width >= CH_WIDTH_320MHZ) {
 			pe_debug("Dropping req: invalid wide_bw IE");
 			return eRRM_REFUSED;
 		}
-	} else {
-		pe_debug("IE(s) are NULL in channel load request");
 	}
 
 	op_class = chan_load_req->measurement_request.channel_load.op_class;
@@ -2198,24 +2199,11 @@ rrm_process_channel_load_req(struct mac_context *mac,
 	load_ind->meas_duration = meas_duration;
 	curr_req->token = chan_load_req->measurement_token;
 
-	if (is_wide_bw_chan_switch) {
-		load_ind->wide_bw.is_wide_bw_chan_switch = true;
-		load_ind->wide_bw.channel_width = wide_bw.channel_width;
-		load_ind->wide_bw.center_chan_freq0 = wide_bw.center_chan_freq0;
-		load_ind->wide_bw.center_chan_freq1 = wide_bw.center_chan_freq1;
-	} else {
-		load_ind->wide_bw.channel_width = CH_WIDTH_INVALID;
-	}
+	if (is_wide_bw_chan_switch)
+		load_ind->wide_bw = wide_bw;
 
-	if (bw_ind.is_bw_ind_element) {
-		load_ind->bw_ind.is_bw_ind_element = true;
-		load_ind->bw_ind.channel_width = bw_ind.channel_width;
-		load_ind->bw_ind.ccfi0 = bw_ind.ccfi0;
-		load_ind->bw_ind.ccfi1 = bw_ind.ccfi1;
-		load_ind->bw_ind.center_freq = bw_ind.center_freq;
-	} else {
-		load_ind->bw_ind.is_bw_ind_element = false;
-	}
+	if (is_bw_ind)
+		load_ind->bw_ind = bw_ind;
 
 	/* Send request to SME. */
 	msg.type = eWNI_SME_CHAN_LOAD_REQ_IND;
