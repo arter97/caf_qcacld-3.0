40bb77139a84f9f74569da44c8b94533e3c2c5cd
Divya R <quic_divraj@quicinc.com>
2024-02-14 09:50:19 +0530

    qca-wifi: Add new api to get client PSD based on Indoor SP AP
    New api 'wlan_reg_get_client_psd_for_compap' added to get the
    client PSD for Indoor SP AP
    
    PSD for LPI = MAX(LPI Client Power , SP Client Power)
    
    Change-Id: I53dd0588edacf7a524744db6eda6a8d638607660

--- umac/regulatory/dispatcher/src/wlan_reg_channel_api.c
+++ umac/regulatory/dispatcher/src/wlan_reg_channel_api.c
@@ -310,6 +310,26 @@ wlan_reg_get_client_psd_for_ap(struct wlan_objmgr_pdev *pdev,
 	return reg_get_client_psd_for_ap(pdev, ap_pwr_type, client_type,
 					 chan_freq, reg_psd);
 }
+QDF_STATUS
+wlan_reg_get_client_psd_for_compap(struct wlan_objmgr_pdev *pdev,
+				   enum reg_6g_ap_type ap_pwr_type,
+				   enum reg_6g_client_type client_type,
+				   qdf_freq_t chan_freq,
+				   int16_t *reg_psd)
+{
+	int16_t max_reg_psd_lpi, max_reg_psd_sp;
+	QDF_STATUS status;
+
+	status = reg_get_client_psd_for_ap(pdev, ap_pwr_type,
+					   client_type,
+					   chan_freq, &max_reg_psd_lpi);
+	status = reg_get_client_psd_for_ap(pdev, REG_STANDARD_POWER_AP,
+					   client_type,
+					   chan_freq, &max_reg_psd_sp);
+
+	*reg_psd = QDF_MAX((int8_t)max_reg_psd_lpi, (int8_t)max_reg_psd_sp);
+	return status;
+}
 #endif
 
 struct regulatory_channel
