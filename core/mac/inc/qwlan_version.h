4d089edeb7b0c9e23682c35038883a273cb93b0c
Ravindra Konda <quic_konda@quicinc.com>
2024-03-18 01:05:35 -0700

    Release 5.2.1.93F
    Release 5.2.1.93F
    
    Change-Id: I266a6e03f82107d4a329e257fbfe7c407db65eb0
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "E"
+#define QWLAN_VERSION_EXTRA            "F"
 #define QWLAN_VERSION_BUILD            93
 
-#define QWLAN_VERSIONSTR               "5.2.1.93E"
+#define QWLAN_VERSIONSTR               "5.2.1.93F"
 
 #endif /* QWLAN_VERSION_H */
