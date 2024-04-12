d30bfdd2844776b1bdf31f6b72da89ba2fb8010d
Liangwei Dong <quic_liangwei@quicinc.com>
2024-04-12 14:50:37 +0800

    qcacld-3.0: Update channel info for mlo mgr links
    lim_check_scan_db_for_join_req_partner_info will be invoked
    when ml probe response doesn't include expected partner link
    info. From the latest logic, if the scan entry can be found
    for the partner links, we still allow the partner links in
    association. But we need to update channel info in mlo link
    info for partner links.
    Fix by call lim_update_mlo_mgr_info to update channel info
    in mlo link info.
    
    Change-Id: I20e9d333ab9feac09aabec437fe853ccfbdbfafb
    CRs-Fixed: 3783911

--- core/mac/src/pe/lim/lim_api.c
+++ core/mac/src/pe/lim/lim_api.c
@@ -4089,8 +4089,8 @@ lim_check_scan_db_for_join_req_partner_info(struct pe_session *session_entry,
 	struct wlan_objmgr_pdev *pdev;
 	struct mlo_partner_info *partner_info;
 	struct mlo_link_info *link_info;
-	struct scan_cache_entry *partner_entry;
 	uint8_t i;
+	QDF_STATUS status;
 
 	if (!session_entry) {
 		pe_err("session entry is NULL");
@@ -4117,17 +4117,17 @@ lim_check_scan_db_for_join_req_partner_info(struct pe_session *session_entry,
 	partner_info = &lim_join_req->partner_info;
 	for (i = 0; i < partner_info->num_partner_links; i++) {
 		link_info = &partner_info->partner_link_info[i];
-		partner_entry =
-			wlan_scan_get_entry_by_bssid(pdev,
-						     &link_info->link_addr);
-		if (!partner_entry) {
-			pe_err("Scan entry is not found for partner link %d "
-			       QDF_MAC_ADDR_FMT,
-			       link_info->link_id,
-			       QDF_MAC_ADDR_REF(link_info->link_addr.bytes));
+		status = lim_update_mlo_mgr_info(mac_ctx,
+						 session_entry->vdev,
+						 &link_info->link_addr,
+						 link_info->link_id,
+						 link_info->chan_freq);
+		if (QDF_IS_STATUS_ERROR(status)) {
+			pe_err("failed %d to update mlo_mgr link id %d freq %d",
+			       status, link_info->link_id,
+			       link_info->chan_freq);
 			return QDF_STATUS_E_FAILURE;
 		}
-		util_scan_free_cache_entry(partner_entry);
 	}
 
 	return QDF_STATUS_SUCCESS;
