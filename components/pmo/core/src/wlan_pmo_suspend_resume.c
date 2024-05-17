3936580b2372dd5709e0ba53d8ea66f47af26fe7
Arun Kumar Khandavalli <quic_akhandav@quicinc.com>
2024-05-17 17:21:16 +0530

    qcacld-3.0: get the hif handle directly rather than with spinlock
    Currently nack status is read using the pmo lock with bottom half
    disabled, but if the wow wakeup irq is triggered at the sametime
    it is waiting for the same psoc lock resulting in the dead lock.
    
    Hence get the hif handle directly rather than with spinlock api.
    
    CRs-Fixed: 3798619
    Change-Id: Ic2d5cd07c2dafb525003ec7e9e02bc17d0876dd0

--- components/pmo/core/src/wlan_pmo_suspend_resume.c
+++ components/pmo/core/src/wlan_pmo_suspend_resume.c
@@ -1690,13 +1690,13 @@ void pmo_core_psoc_handle_initial_wake_up(void *cb_ctx)
 		return;
 	}
 
-	hif_ctx = pmo_core_psoc_get_hif_handle(psoc);
+	psoc_ctx = pmo_psoc_get_priv(psoc);
+	hif_ctx = psoc_ctx->hif_hdl;
 	if (!hif_ctx)
 		pmo_err("hif ctx is null, request resume not called");
 	else if(hif_pm_get_wake_irq_type(hif_ctx) == HIF_PM_CE_WAKE)
 		hif_rtpm_check_and_request_resume(true);
 
-	psoc_ctx = pmo_psoc_get_priv(psoc);
 	pmo_core_update_wow_initial_wake_up(psoc_ctx, 1);
 }
 
