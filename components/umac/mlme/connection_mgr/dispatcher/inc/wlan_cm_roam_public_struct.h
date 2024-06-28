c1124df3f19afbeeb798cdea6e3cda05658f19e3
Srinivas Dasari <quic_dasaris@quicinc.com>
2024-06-28 16:11:19 +0530

    qcacld-3.0: Isolate roam channel update from UPDATE_CFG
    Currently, all roam params are updated to firmware whenever
    a new param(e.g. preferred channels) is configured by the
    userspace. This may overwrite the firmware learned data with
    fresh config which is not intended.
    Isolate the configured param update to firmware to avoid
    tampering other params.
    As there are multiple roam params, isolate ROAM CHAN configuration
    for now. Incremental changes can be added to make other params
    adapt to this.
    
    Change-Id: Ib8c01e42263a0d77b5747f71591ac282909ed7d3
    CRs-Fixed: 3856863

--- components/umac/mlme/connection_mgr/dispatcher/inc/wlan_cm_roam_public_struct.h
+++ components/umac/mlme/connection_mgr/dispatcher/inc/wlan_cm_roam_public_struct.h
@@ -2644,6 +2644,7 @@ struct roam_pmkid_req_event {
  * @send_roam_mlo_config: send MLO config to FW
  * @send_roam_scan_offload_rssi_params: Set the RSSI parameters for roam
  * offload scan
+ * @send_roam_frequencies: send roam frequencies to FW
  */
 struct wlan_cm_roam_tx_ops {
 	QDF_STATUS (*send_vdev_set_pcl_cmd)(struct wlan_objmgr_vdev *vdev,
@@ -2702,6 +2703,9 @@ struct wlan_cm_roam_tx_ops {
 	QDF_STATUS (*send_roam_mlo_config)(struct wlan_objmgr_vdev *vdev,
 					   struct wlan_roam_mlo_config *req);
 #endif
+	QDF_STATUS (*send_roam_frequencies)(
+			struct wlan_objmgr_vdev *vdev,
+			struct wlan_roam_scan_channel_list *rso_ch_info);
 };
 
 /**
