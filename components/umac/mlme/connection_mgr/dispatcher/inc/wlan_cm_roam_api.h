110465f5c183e49bdbe57a40609ab62151a58313
Chaoli Zhou <quic_zchaoli@quicinc.com>
2024-07-19 15:17:01 +0800

    qcacld-3.0: Clear cached sae offload bssid in the disconnect state
    Need to clear the sae offload bssid while STA DUT
    happen disconnection, otherwise in the next sae
    roaming case, it will miss the chance to get the latest
    candidate information from scan DB and may not update the
    correct SSID to sae external auth event to wpa_supplicant
    that finally cause the pre-auth failure.
    eg:
    STA DUT connect to AP1 with WPA2-PSK->
    STA DUT roaming to AP2 with SAE->
    STA DUT store sae offload bssid after received roam
    pre-auth event from wlan fw in the
    wlan_cm_update_offload_ssid_from_candidate()->
    wpa_supplicant get correct bssid and ssid for external auth->
    STA DUT disconnect from AP2->
    STA DUT connect to AP1 again->
    STA DUT roaming to AP2 with SAE->
    STA DUT receive roam pre-auth event from wlan fw->
    STA DUT get old cached bssid and skip ssid update->
    wpa_supplicant not get correct ssid for external auth
    
    Change-Id: I9ac159af6b9a87d27924e2f230fb651356ecf701
    CRs-Fixed: 3874691

--- components/umac/mlme/connection_mgr/dispatcher/inc/wlan_cm_roam_api.h
+++ components/umac/mlme/connection_mgr/dispatcher/inc/wlan_cm_roam_api.h
@@ -1149,6 +1149,16 @@ wlan_cm_get_roam_offload_bssid(struct wlan_objmgr_vdev *vdev,
 			       struct qdf_mac_addr *bssid);
 
 /**
+ * wlan_cm_clear_roam_offload_bssid() - Clear the roam offload bssid of the sae
+ * roam candidate
+ * @vdev: pointer to vdevs
+ *
+ * Return: None
+ */
+void
+wlan_cm_clear_roam_offload_bssid(struct wlan_objmgr_vdev *vdev);
+
+/**
  * wlan_cm_set_roam_offload_ssid() - Set the roam offload candidate ssid
  *
  * @vdev: pointer to vdev
@@ -1390,6 +1400,11 @@ wlan_cm_set_roam_offload_bssid(struct wlan_objmgr_vdev *vdev,
 static inline void
 wlan_cm_get_roam_offload_bssid(struct wlan_objmgr_vdev *vdev,
 			       struct qdf_mac_addr *bssid)
+{
+}
+
+static inline void
+wlan_cm_clear_roam_offload_bssid(struct wlan_objmgr_vdev *vdev)
 {
 }
 
