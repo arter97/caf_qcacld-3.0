c3c5de6d9d759c5571652b1222f40a1647c1971b
Liangwei Dong <quic_liangwei@quicinc.com>
2024-08-19 23:00:57 -0700

    qcacld-3.0: Update dfs phyerr offload state before vdev start
    The current code will update dfs phyerr offload after vdev start
    response of AP. But firmware expects the value to be updated
    before vdev start.
    
    Fix by remove the code of update the dfs phyerr offload after
    vdev start response and add it to vdev start request.
    
    Change-Id: Ie1bd9130094689ad72f793bf5274b146fa559408
    CRs-Fixed: 3900192

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -1354,12 +1354,6 @@ QDF_STATUS wma_vdev_start_resp_handler(struct vdev_mlme_obj *vdev_mlme,
 			  rsp->vdev_id, rsp->cfgd_tx_streams,
 			  rsp->cfgd_rx_streams, rsp->chain_mask, mac_id);
 
-		if (wma_is_vdev_in_ap_mode(wma, rsp->vdev_id))
-			policy_mgr_update_dfs_master_dynamic_enabled(
-					psoc,
-					true,
-					iface->vdev->vdev_mlme.des_chan);
-
 		/* Fill bss_chan after vdev start */
 		qdf_mem_copy(iface->vdev->vdev_mlme.bss_chan,
 			     iface->vdev->vdev_mlme.des_chan,
