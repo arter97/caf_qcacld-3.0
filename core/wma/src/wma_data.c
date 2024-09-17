69031f7614247c6bede2e32417c9830e18592edb
Vinod Kumar Myadam <quic_vmyadam@quicinc.com>
2024-09-16 22:44:47 -0700

    qcacld-3.0: Fix for freq to chan mapping in set time quota
    Currently for frequency to channel mapping in set time quota
    command using the legacy API which support 2.4 GHz and 5 GHz
    bands. For 6 GHz band returns the invalid channel which leads
    to crash in FW.
    
    Change the legacy API to regulatory API supports all the bands
    and while posting message to scheduler add band mask for the two
    frequencies to help in convert back from channel to frequency.
    
    Change-Id: If4038125ffaab38d057b0d97444cd1269fde18db
    CRs-Fixed: 3921964

--- core/wma/src/wma_data.c
+++ core/wma/src/wma_data.c
@@ -1182,6 +1182,8 @@ QDF_STATUS wma_set_mcc_channel_time_latency(tp_wma_handle wma,
  * @adapter_1_chan_number: adapter 1 channel number
  * @adapter_1_quota: adapter 1 quota
  * @adapter_2_chan_number: adapter 2 channel number
+ * @band_1: Band bitmap for @adapter_1_chan_number
+ * @band_2: Band bitmap for @adapter_2_chan_number
  *
  * Currently used to set time quota for 2 MCC vdevs/adapters using (operating
  * channel, quota) for each mode . The info is provided run time using
@@ -1194,12 +1196,12 @@ QDF_STATUS wma_set_mcc_channel_time_latency(tp_wma_handle wma,
  */
 QDF_STATUS wma_set_mcc_channel_time_quota(tp_wma_handle wma,
 		uint32_t adapter_1_chan_number,	uint32_t adapter_1_quota,
-		uint32_t adapter_2_chan_number)
+		uint32_t adapter_2_chan_number, uint8_t band_1, uint8_t band_2)
 {
 	bool mcc_adapt_sch = false;
 	struct mac_context *mac = NULL;
-	uint32_t chan1_freq = cds_chan_to_freq(adapter_1_chan_number);
-	uint32_t chan2_freq = cds_chan_to_freq(adapter_2_chan_number);
+	uint32_t chan1_freq;
+	uint32_t chan2_freq;
 
 	if (!wma) {
 		wma_err("NULL wma ptr. Exiting");
@@ -1218,6 +1220,13 @@ QDF_STATUS wma_set_mcc_channel_time_quota(tp_wma_handle wma,
 		QDF_ASSERT(0);
 		return QDF_STATUS_E_FAILURE;
 	}
+
+	chan1_freq = wlan_reg_chan_band_to_freq(wma->pdev,
+						adapter_1_chan_number,
+						band_1);
+	chan2_freq = wlan_reg_chan_band_to_freq(wma->pdev,
+						adapter_2_chan_number,
+						band_2);
 
 	/* Confirm MCC adaptive scheduler feature is disabled */
 	if (policy_mgr_get_dynamic_mcc_adaptive_sch(mac->psoc,
