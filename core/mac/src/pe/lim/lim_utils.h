687829193e0b69367ac309c936e04f092ed41687
Surya Prakash Sivaraj <quic_suryapra@quicinc.com>
2024-09-30 17:40:11 +0530

    qcacld-3.0: Send PMKID in assoc request
    Do not prevent sending PMKID in the assoc request. The OUI
    to check the IOT AP is too generic, and APs with same OUI is
    present in the WFA testbeds.
    
    This reverts the commit I85f39f52e4dae8393bbf559969710f35447b8c0c
    
    Change-Id: I63a36ce6fdadc064ebc2fc4fb9c609162124de2a
    CRs-Fixed: 3936362

--- core/mac/src/pe/lim/lim_utils.h
+++ core/mac/src/pe/lim/lim_utils.h
@@ -1101,7 +1101,6 @@ QDF_STATUS lim_send_ies_per_band(struct mac_context *mac_ctx,
 
 /**
  * lim_update_connect_rsn_ie() - Update the connection RSN IE
- * @mac_ctx: MAC context
  * @session: PE session
  * @rsn_ie_buf: RSN IE buffer
  * @pmksa: PMKSA entry for the connecting AP
@@ -1109,8 +1108,7 @@ QDF_STATUS lim_send_ies_per_band(struct mac_context *mac_ctx,
  * Return: None
  */
 void
-lim_update_connect_rsn_ie(struct mac_context *mac_ctx,
-			  struct pe_session *session, uint8_t *rsn_ie_buf,
+lim_update_connect_rsn_ie(struct pe_session *session, uint8_t *rsn_ie_buf,
 			  struct wlan_crypto_pmksa *pmksa);
 
 /**
