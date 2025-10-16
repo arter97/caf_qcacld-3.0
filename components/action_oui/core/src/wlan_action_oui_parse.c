067111c3f485f13100f97fc2889ab924ea7fbe33
Shoaib Ahmed <shoahme@qti.qualcomm.com>
2025-10-16 21:40:29 +0530

    qcacld-3.0: Fix FORWARD_NULL in populate_dot11f_assoc_req_mlo_ie
    A FORWARD_NULL issue was reported by the static analysis tool in the
    `populate_dot11f_assoc_req_mlo_ie` function. This was caused by
    dereferencing `pe_session->lim_join_req` before its null check.
    Additionally, `attr.mac_addr` was not validated, leading to a
    potential null pointer dereference during the `wlan_action_oui_search`
    call.
    
    This change resolves the issue by:
    1.  Moving the null check for `pe_session->lim_join_req` to the
        beginning of the function, before any dereferencing occurs.
    2.  Added the null check for `attr.mac_addr`.
    
    CRs-Fixed: 4319894
    Change-Id: Ia10da2218ae0b322dbaa258672f94c2537ac1346

--- components/action_oui/core/src/wlan_action_oui_parse.c
+++ components/action_oui/core/src/wlan_action_oui_parse.c
@@ -890,7 +890,7 @@ check_for_vendor_ap_mac(struct action_oui_extension *extension,
 	uint8_t *mac_addr = attr->mac_addr;
 
 	if (!mac_addr)
-		return true;
+		return false;
 
 	for (i = 0; i < QDF_MAC_ADDR_SIZE; i++) {
 		if ((*extension->mac_mask & mac_mask) &&
